f0024ec9b088d210c3fc2b24667330fa642aebab
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2009-12-16 12:09:59 +0000

    added startup.txt

--- src/vetcontrol-web-server/src/main/java/org/vetcontrol/web/pages/StartPage.java
+++ src/vetcontrol-web-server/src/main/java/org/vetcontrol/web/pages/StartPage.java
@@ -7,6 +7,8 @@ package org.vetcontrol.web.pages;
 import java.util.Arrays;
 import java.util.Locale;
 import org.apache.wicket.markup.html.WebPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.vetcontrol.web.components.LocalePicker;
 
 /**
@@ -15,7 +17,12 @@ import org.vetcontrol.web.components.LocalePicker;
  */
 public class StartPage extends WebPage {
 
+    private static final Logger log = LoggerFactory.getLogger(StartPage.class);
+
     public StartPage() {
+
+        log.info("Start application");
+
         add(new LocalePicker("localePicker", Arrays.asList(new Locale[]{Locale.ENGLISH, new Locale("ru")})));
     }
 }
