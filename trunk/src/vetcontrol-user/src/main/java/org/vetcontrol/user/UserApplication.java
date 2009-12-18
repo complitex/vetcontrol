742faf6bf1cd3f3fb63eebe7b2f60ed06099b082
heaven.iaa <heaven.iaa@1d30cab6-e88b-11de-b362-3513cf04e977>
2009-12-18 13:11:44 +0000

    optimize servlet  authentication stratagy

--- trunk/src/vetcontrol-user/src/main/java/org/vetcontrol/user/UserApplication.java
+++ trunk/src/vetcontrol-user/src/main/java/org/vetcontrol/user/UserApplication.java
@@ -1,13 +1,13 @@
 package org.vetcontrol.user;
 
 import org.apache.wicket.Page;
-import org.vetcontrol.web.login.BaseLoginApplication;
+import org.vetcontrol.web.security.ServeltAuthWebApplication;
 
 /**
  * User: Anatoly A. Ivanov java@inheaven.ru
  * Date: 16.12.2009 21:30:40
  */
-public class UserApplication extends BaseLoginApplication{
+public class UserApplication extends ServeltAuthWebApplication {
     @Override
     public Class<? extends Page> getHomePage() {
         return UserHomePage.class; 
