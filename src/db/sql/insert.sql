4f518eae14bde9d679c740ab40dc46f965aaa917
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2009-12-18 08:25:14 +0000

    moved db to top project

---
+++ src/db/sql/insert.sql
@@ -0,0 +1,48 @@
+INSERT INTO project1.`roles` (`name`) VALUES ('ADMIN');
+INSERT INTO project1.roles (`name`) VALUES ('DONT_EDIT_ADMIN');
+
+INSERT INTO project1.`user`
+        (`name`,
+	`login`,
+	`password`,
+	`parentNodeId`,
+	`blocked`
+	)
+	VALUES
+	('ADMIN',
+	'ADMIN',
+	'ADMIN',
+	null,
+	0
+	);
+INSERT INTO project1.`user`
+        (`name`,
+	`login`,
+	`password`,
+	`parentNodeId`,
+	`blocked`
+	)
+	VALUES
+	('DONT_EDIT_ADMIN',
+	'DONT_EDIT_ADMIN',
+	'DONT_EDIT_ADMIN',
+	null,
+	0
+	);
+
+INSERT INTO project1.`role`
+	(`role`,
+	`userName`
+	)
+	VALUES
+	('ADMIN',
+	'ADMIN'
+	);
+INSERT INTO project1.`role`
+	(`role`,
+	`userName`
+	)
+	VALUES
+	('DONT_EDIT_ADMIN',
+	'DONT_EDIT_ADMIN'
+	);
