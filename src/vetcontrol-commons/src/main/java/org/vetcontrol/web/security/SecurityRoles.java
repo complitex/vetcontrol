5f8d8b8c98d79504ccfdf78049baa21730c57767
heaven.iaa <heaven.iaa@1d30cab6-e88b-11de-b362-3513cf04e977>
2009-12-23 12:26:40 +0000

    template menu configuration

--- src/vetcontrol-commons/src/main/java/org/vetcontrol/web/security/SecurityRoles.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/web/security/SecurityRoles.java
@@ -4,10 +4,12 @@ package org.vetcontrol.web.security;
  * User: Anatoly A. Ivanov java@inheaven.ru
  * Date: 19.12.2009 0:43:11
  */
-public enum SecurityRoles {
-    AUTHORIZED,
-    USER_EDIT,
-    INFORMATION_EDIT,
-    DOCUMENT_CREATE, DOCUMENT_EDIT, DOCUMENT_VIEW_DEPARTMENT, DOCUMENT_EDIT_DEPARTMENT,
-    REPORT_CREATE, REPORT_EDIT, REPORT_VIEW_DEPARTMENT, REPORT_EDIT_DEPARTMENT
+public class SecurityRoles {
+    public final static String AUTHORIZED = "AUTHORIZED";
+    public final static String USER_EDIT = "USER_EDIT";
+    public final static String INFORMATION_EDIT = "INFORMATION_EDIT";            
+
+//    DOCUMENT_CREATE, DOCUMENT_EDIT, DOCUMENT_VIEW_DEPARTMENT, DOCUMENT_EDIT_DEPARTMENT,
+//    REPORT_CREATE, REPORT_EDIT, REPORT_VIEW_DEPARTMENT, REPORT_EDIT_DEPARTMENT
+
 }
--- src/vetcontrol-web-server/src/main/java/org/vetcontrol/server/web/VetcontrolWebApplication.java
+++ src/vetcontrol-web-server/src/main/java/org/vetcontrol/server/web/VetcontrolWebApplication.java
@@ -1,52 +1,24 @@
 package org.vetcontrol.server.web;
 
-import java.util.ArrayList;
-import org.apache.wicket.Component;
 import org.apache.wicket.Page;
-import org.apache.wicket.util.resource.IResourceStream;
-import org.apache.wicket.util.resource.UrlResourceStream;
-import org.vetcontrol.server.web.pages.HomePage;
-import org.vetcontrol.server.web.template.Template;
-import org.vetcontrol.web.template.TemplateWebApplication;
-
-import java.util.List;
 import org.odlabs.wiquery.core.commons.WiQueryInstantiationListener;
-import org.vetcontrol.server.web.component.AvailableBooksPanel;
-import org.wicketstuff.javaee.injection.JavaEEComponentInjector;
+import org.vetcontrol.server.web.pages.HomePage;
+import org.vetcontrol.web.template.TemplateWebApplication;
 
 /**
  * User: Anatoly A. Ivanov java@inheaven.ru
  * Date: 20.12.2009 23:56:14
  */
-public class VetcontrolWebApplication extends TemplateWebApplication{
-
-    private List<Component> templateComponents;
+public class VetcontrolWebApplication extends TemplateWebApplication {
 
     @Override
     protected void init() {
-        addComponentInstantiationListener(new WiQueryInstantiationListener());
-        addComponentInstantiationListener(new JavaEEComponentInjector(this));
         super.init();
-
-        // TODO: add locale support, menu visible by role, header info
-    }
-
-    @Override
-    public List<Component> getTemplateComponents() {        
-        templateComponents = new ArrayList<Component>();
-        templateComponents.add(new AvailableBooksPanel("BooksPanel"));
-
-        return templateComponents;
-    }
-
-    @Override
-    public IResourceStream getTemplateMarkup() {
-        return new UrlResourceStream(Template.class.getResource("template.html"));
+        addComponentInstantiationListener(new WiQueryInstantiationListener());
     }
 
     @Override
     public Class<? extends Page> getHomePage() {
         return HomePage.class;
     }
-
 }
