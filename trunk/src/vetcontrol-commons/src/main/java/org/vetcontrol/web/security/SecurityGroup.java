80060b9302f79467215d08d81be3f11063db526f
heaven.iaa <heaven.iaa@1d30cab6-e88b-11de-b362-3513cf04e977>
2009-12-24 22:39:12 +0000

    user add, user edit, client-web and server web merged to module web

---
+++ trunk/src/vetcontrol-commons/src/main/java/org/vetcontrol/web/security/SecurityGroup.java
@@ -0,0 +1,9 @@
+package org.vetcontrol.web.security;
+
+/**
+ * User: Anatoly A. Ivanov java@inheaven.ru
+ * Date: 21.12.2009 16:00:23
+ */
+public enum SecurityGroup {
+    ADMINISTRATORS, DEPARTMENT_OFFICERS, LOCAL_OFFICERS, MOBILE_OFFICERS
+}
