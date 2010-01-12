c12e07a7d67cea599fbd49cd22309075eb1773c4
heaven.iaa <heaven.iaa@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-01-12 08:53:35 +0000

    information entity

---
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/CargoType.java
@@ -0,0 +1,53 @@
+package org.vetcontrol.entity;
+
+import org.vetcontrol.util.book.entity.annotation.MappedProperty;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 12.01.2010 14:31:39
+ *
+ * виды грузов (для отражения в отчетности)
+ */
+//@Entity
+//@Table(name = "cargo_type")
+public class CargoType {
+
+    private Integer id;
+    private Long name;
+    private List<StringCulture> names = new ArrayList<StringCulture>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public Long getName() {
+        return name;
+    }
+
+    public void setName(Long name) {
+        this.name = name;
+    }
+
+    @Transient
+    @MappedProperty("name")
+    @Column(length = 20, nullable = false)
+    public List<StringCulture> getNames() {
+        return names;
+    }
+
+    public void setNames(List<StringCulture> names) {
+        this.names = names;
+    }
+}
---
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/Producer.java
@@ -0,0 +1,51 @@
+package org.vetcontrol.entity;
+
+import org.vetcontrol.util.book.entity.annotation.MappedProperty;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 12.01.2010 14:41:47
+ */
+
+//@Entity
+//@Table(name = "producer")
+public class Producer {
+    private Integer id;
+    private Long name;
+    private List<StringCulture> names = new ArrayList<StringCulture>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public Long getName() {
+        return name;
+    }
+
+    public void setName(Long name) {
+        this.name = name;
+    }
+
+    @Transient
+    @MappedProperty("name")
+    @Column(length = 20, nullable = false)
+    public List<StringCulture> getNames() {
+        return names;
+    }
+
+    public void setNames(List<StringCulture> names) {
+        this.names = names;
+    }    
+}
