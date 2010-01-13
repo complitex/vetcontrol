2b1c680c97cd964343b8f97a19412db03e16b05f
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-01-13 07:22:44 +0000

    added boolean support to information module, added 'controled' property to CargoType book.

---
+++ src/vetcontrol-information/src/main/java/org/vetcontrol/information/web/component/BooleanPanel.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.vetcontrol.information.web.component;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.vetcontrol.information.web.model.DisplayPropertyLocalizableModel;
+import org.vetcontrol.util.book.Property;
+
+/**
+ *
+ * @author Artem
+ */
+public final class BooleanPanel extends Panel {
+    public BooleanPanel(String id, IModel model, Property prop) {
+        super (id);
+
+        IModel labelModel = new DisplayPropertyLocalizableModel(prop, this);
+
+        FormComponent checkBox = new CheckBox("checkbox", model).
+                setLabel(labelModel);
+//                setRequired(!prop.isNullable());
+
+        add(checkBox);
+    }
+}
