445842768347d2c11cf3241c17be93f3f82bbfa2
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-01-21 09:52:07 +0000

    added CargoMode book.

---
+++ trunk/src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/CargoMode.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vetcontrol.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.vetcontrol.util.book.entity.annotation.BookReference;
+
+/**
+ *
+ * @author Artem
+ */
+@Entity
+@Table(name = "cargo_mode")
+public class CargoMode extends Localizable {
+
+    private CargoType cargoType;
+
+    /**
+     * Get the value of cargoType
+     *
+     * @return the value of cargoType
+     */
+    @BookReference(referencedProperty = "names")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
+    @JoinColumn(name = "cargo_type", nullable = false)
+    public CargoType getCargoType() {
+        return cargoType;
+    }
+
+    /**
+     * Set the value of cargoType
+     *
+     * @param cargoType new value of cargoType
+     */
+    public void setCargoType(CargoType cargoType) {
+        this.cargoType = cargoType;
+    }
+
+    private UnitType unitType;
+
+    /**
+     * Get the value of unitType
+     *
+     * @return the value of unitType
+     */
+    @BookReference(referencedProperty = "names")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
+    @JoinColumn(name = "unit_type", nullable = false)
+    public UnitType getUnitType() {
+        return unitType;
+    }
+
+    /**
+     * Set the value of unitType
+     *
+     * @param unitType new value of unitType
+     */
+    public void setUnitType(UnitType unitType) {
+        this.unitType = unitType;
+    }
+
+
+}
