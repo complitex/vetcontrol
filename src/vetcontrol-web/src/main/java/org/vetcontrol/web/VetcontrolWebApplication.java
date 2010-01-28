3550915192da66a10d24f863918b4683d6aad58f
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-01-28 11:45:00 +0000

    added custom page for displaying session expired message.

--- src/vetcontrol-web/src/main/java/org/vetcontrol/web/VetcontrolWebApplication.java
+++ src/vetcontrol-web/src/main/java/org/vetcontrol/web/VetcontrolWebApplication.java
@@ -2,6 +2,7 @@ package org.vetcontrol.web;
 
 import org.apache.wicket.Page;
 import org.vetcontrol.web.pages.HomePage;
+import org.vetcontrol.web.pages.SessionExpiredPage;
 import org.vetcontrol.web.template.TemplateWebApplication;
 
 /**
@@ -11,7 +12,14 @@ import org.vetcontrol.web.template.TemplateWebApplication;
 public class VetcontrolWebApplication extends TemplateWebApplication {
 
     @Override
+    protected void init() {
+        getApplicationSettings().setPageExpiredErrorPage(SessionExpiredPage.class);
+        super.init();
+    }
+
+    @Override
     public Class<? extends Page> getHomePage() {
         return HomePage.class;
     }
+
 }
