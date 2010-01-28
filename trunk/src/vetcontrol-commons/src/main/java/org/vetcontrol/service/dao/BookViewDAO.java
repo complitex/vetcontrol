1c4b36fe066ac5dd9e4c917694fb89dd3b720c8f
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-01-28 06:27:34 +0000

    removed commented code.

--- trunk/src/vetcontrol-commons/src/main/java/org/vetcontrol/service/dao/BookViewDAO.java
+++ trunk/src/vetcontrol-commons/src/main/java/org/vetcontrol/service/dao/BookViewDAO.java
@@ -8,7 +8,6 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
