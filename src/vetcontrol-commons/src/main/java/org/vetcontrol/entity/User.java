968fdaecba830797f3bd7c13d1f9342037f7eac4
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-02-08 04:34:47 +0000

    fixed User hashCode.

--- src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/User.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/User.java
@@ -18,39 +18,28 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @Column(name = "login", length = 32, unique = true, nullable = false)
     private String login;
-
     @Column(name = "_password", length = 32, nullable = false)
     private String password;
-
     @Transient
     private String changePassword;
-
     @Column(name = "first_name", length = 45)
     private String firstName;
-
     @Column(name = "middle_name", length = 45)
     private String middleName;
-
     @Column(name = "last_name", length = 45)
     private String lastName;
-
     @ManyToOne
     @JoinColumn(name = "job_id")
     private Job job;
-
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserGroup> userGroups = new ArrayList<UserGroup>();
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "department_id", nullable = true)
     private Department department;
-
     @Column(name = "locale", length = 2)
     private String locale;
-
     @Column(name = "page_size")
     private Integer pageSize;
 
@@ -152,25 +141,21 @@ public class User implements Serializable {
 
     @Override
     public String toString() {
-        return new StringBuilder().append("[hash: ").append(Integer.toHexString(hashCode())).append(", id: ")
-                .append(id).append(", login: ").append(login).append(", firstName: ")
-                .append(firstName).append(", lastName: ").append(lastName).append(", middleName: ")
-                .append(middleName).append(", userGroups: ").append(userGroups).append(", department: ")
-                .append(department).append("]").toString();
+        return new StringBuilder().append("[hash: ").append(Integer.toHexString(hashCode())).append(", id: ").append(id).append(", login: ").append(login).append(", firstName: ").append(firstName).append(", lastName: ").append(lastName).append(", middleName: ").append(middleName).append(", userGroups: ").append(userGroups).append(", department: ").append(department).append("]").toString();
     }
 
     @Transient
-    public String getFullName(){
+    public String getFullName() {
         return ((lastName != null) ? lastName + " " : "")
                 + ((firstName != null) ? firstName + " " : "")
-                + ((middleName != null) ? middleName: "");
+                + ((middleName != null) ? middleName : "");
     }
 
     @Transient
-    public String getShortName(){
-         return ((lastName != null) ? lastName + " " : "")
-                + ((firstName != null) ? firstName.substring(0,1) + ". " : "")
-                + ((middleName != null) ? middleName.substring(0,1): ".");           
+    public String getShortName() {
+        return ((lastName != null) ? lastName + " " : "")
+                + ((firstName != null) ? firstName.substring(0, 1) + ". " : "")
+                + ((middleName != null) ? middleName.substring(0, 1) : ".");
     }
 
     @Override
@@ -200,11 +185,15 @@ public class User implements Serializable {
         if ((this.lastName == null) ? (other.lastName != null) : !this.lastName.equals(other.lastName)) {
             return false;
         }
-        if (this.job.getId() != other.job.getId() && (this.job.getId() == null || !this.job.getId().equals(other.job.getId()))) {
-            return false;
+        if (this.job != null && other.job != null) {
+            if (this.job.getId() != other.job.getId() && (this.job.getId() == null || !this.job.getId().equals(other.job.getId()))) {
+                return false;
+            }
         }
-        if (this.department.getId() != other.department.getId() && (this.department.getId() == null || !this.department.getId().equals(other.department.getId()))) {
-            return false;
+        if (this.department != null && other.department != null) {
+            if (this.department.getId() != other.department.getId() && (this.department.getId() == null || !this.department.getId().equals(other.department.getId()))) {
+                return false;
+            }
         }
         return true;
     }
@@ -218,10 +207,8 @@ public class User implements Serializable {
         hash = 37 * hash + (this.firstName != null ? this.firstName.hashCode() : 0);
         hash = 37 * hash + (this.middleName != null ? this.middleName.hashCode() : 0);
         hash = 37 * hash + (this.lastName != null ? this.lastName.hashCode() : 0);
-        hash = 37 * hash + (this.job.getId() != null ? this.job.getId().hashCode() : 0);
-        hash = 37 * hash + (this.department.getId() != null ? this.department.getId().hashCode() : 0);
+        hash = 37 * hash + (this.job != null ? (this.job.getId() != null ? this.job.getId().hashCode() : 0) : 0);
+        hash = 37 * hash + (this.department != null ? (this.department.getId() != null ? this.department.getId().hashCode() : 0) : 0);
         return hash;
     }
-
-
 }
