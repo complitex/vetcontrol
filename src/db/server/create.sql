151d64888b4f60283824b949ac5f3a0dfb18ce3e
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-02-26 11:21:58 +0000

    added `deleted` table.

---
+++ src/db/server/create.sql
@@ -0,0 +1,14 @@
+
+/*Table structure for table `deleted` */
+/* This table contains entries which has been removed from any application table. This information may be useful at client synchronization and logging. */
+DROP TABLE IF EXISTS `deleted`;
+CREATE TABLE `deleted` (
+  /* Id of removed entry. It may be any type but table contains string value of that type. If id is composite id then what this value will be
+        is up to application. For example, concatenating of compound properties. */
+  `id` VARCHAR(32) NOT NULL,
+  /* The table from current entry was removed. */
+  `table` VARCHAR(32) NOT NULL,
+  /* Timestamp of deleting. */
+  `deleted` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
