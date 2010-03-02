f1f1c6074715f5495e577814ecb2cb9db1844ede
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-02 05:00:27 +0000

    renamed deleted table onto deleted_embedded_id, created composite id which compound of concatenated ids of entity and entity name.

--- trunk/src/db/server/create.sql
+++ trunk/src/db/server/create.sql
@@ -1,8 +1,8 @@
 
-/*Table structure for table `deleted` */
+/*Table structure for table `deleted_embedded_id` */
 /* This table contains entries which has been removed from any application table. This information may be useful at client synchronization and logging. */
-DROP TABLE IF EXISTS `deleted`;
-CREATE TABLE `deleted` (
+DROP TABLE IF EXISTS `deleted_embedded_id`;
+CREATE TABLE `deleted_embedded_id` (
   /* Id of removed entry. It may be any type but table contains string value of that type. If id is composite id then what this value will be
         is up to application. For example, concatenating of compound properties. */
   `id` VARCHAR(100) NOT NULL,
@@ -10,5 +10,5 @@ CREATE TABLE `deleted` (
   `entity` VARCHAR(100) NOT NULL,
   /* Timestamp of deleting. */
   `deleted` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`, `entity`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
