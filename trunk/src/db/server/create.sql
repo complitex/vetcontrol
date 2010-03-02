9f5413591141f4d2c8681a1f98011c3ac76ab56d
heaven.iaa <heaven.iaa@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-02 06:55:50 +0000

    VC-20 add copy removed user groups to deleted items table

--- trunk/src/db/server/create.sql
+++ trunk/src/db/server/create.sql
@@ -1,14 +1,27 @@
 
-/*Table structure for table `deleted_embedded_id` */
-/* This table contains entries which has been removed from any application table. This information may be useful at client synchronization and logging. */
+/*Table structure for table `deleted_embedded_id`
+ This table contains entries which has been removed from any application table.
+ This information may be useful at client synchronization and logging. */
 DROP TABLE IF EXISTS `deleted_embedded_id`;
 CREATE TABLE `deleted_embedded_id` (
-  /* Id of removed entry. It may be any type but table contains string value of that type. If id is composite id then what this value will be
-        is up to application. For example, concatenating of compound properties. */
+  /* Id of removed entry. It may be any type but table contains string value of that type.
+     If id is composite id then what this value will be is up to application. For example,
+     concatenating of compound properties. */
   `id` VARCHAR(100) NOT NULL,
   /* The name of entity of removed entry. */
   `entity` VARCHAR(100) NOT NULL,
   /* Timestamp of deleting. */
   `deleted` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`, `entity`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+/* Table structure for table `deleted_embedded_id`
+   This table contains entries which has been removed from any application table.
+   This information may be useful at client synchronization and logging.*/
+DROP TABLE IF EXISTS `deleted_long_id`;
+CREATE TABLE `deleted_long_id` (
+  `id` bigint(20) NOT NULL, /* Long Id of removed entry.*/
+  `entity` VARCHAR(100) NOT NULL, /* The name of entity of removed entry. */
+  `deleted` TIMESTAMP NOT NULL, /* Time stamp of deleting. */
   PRIMARY KEY (`id`, `entity`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
---
+++ trunk/src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/DeletedLongId.java
@@ -0,0 +1,103 @@
+package org.vetcontrol.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 02.03.2010 11:41:07
+ */
+@Entity
+@Table(name = "deleted_long_id")
+public class DeletedLongId  implements Serializable{
+    @Embeddable
+    public static class Id implements Serializable{
+        private Long id;
+        private String entity;
+
+        public Id() {
+        }
+
+        public Id(Long id, String entity) {
+            this.id = id;
+            this.entity = entity;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getEntity() {
+            return entity;
+        }
+
+        public void setEntity(String entity) {
+            this.entity = entity;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+
+            if (!(obj instanceof Id)) {
+                return false;
+            }
+
+            final Id other = (Id) obj;
+
+            if ((id == null) ? (other.id != null) : !id.equals(other.id)) {
+                return false;
+            }
+
+            if ((entity == null) ? (other.entity != null) : !entity.equals(other.entity)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 5;
+            hash = 73 * hash + (this.id != null ? this.id.hashCode() : 0);
+            hash = 73 * hash + (this.entity != null ? this.entity.hashCode() : 0);
+            return hash;
+        }
+    }
+
+    @EmbeddedId
+    private Id id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deleted;
+
+    public DeletedLongId() {
+    }
+
+    public DeletedLongId(Long lid, String entity, Date deleted) {        
+        this.id = new Id(lid, entity);
+        this.deleted = deleted;
+    }
+
+    public Id getId() {
+        return id;
+    }
+
+    public void setId(Id id) {
+        this.id = id;
+    }
+
+    public Date getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Date deleted) {
+        this.deleted = deleted;
+    }
+}
