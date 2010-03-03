bb95fc95d887118cc0df498900e20dd7523b533c
heaven.iaa <heaven.iaa@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-03 06:55:27 +0000

    fix client table create sql

--- trunk/src/db/server/insert.sql
+++ trunk/src/db/server/insert.sql
@@ -1,1 +1,1 @@
-insert into client (`id`, `ip`, `mac`, `secure_key`, `department_id`, `created`) value (1, '0.0.0.0', '00-00-00-00-00-00', 'b2627dab45b9455e947f9755861aea10', 1, NOW());
+insert into client (`id`, `ip`, `mac`, `secure_key`, `department_id`, `created`, `updated`) value (1, '0.0.0.0', '00-00-00-00-00-00', 'b2627dab45b9455e947f9755861aea10', 1, NOW(), NOW());
