e6afc4c98eb325c0f609f707774223bdd8c447f9
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-16 09:39:38 +0000

    DocumentCargoEdit was modified to employ unified AjaxRemovableListView.

--- src/vetcontrol-commons/src/main/java/org/vetcontrol/web/component/list/AjaxRemovableListView.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/web/component/list/AjaxRemovableListView.java
@@ -9,6 +9,9 @@ import java.util.List;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.AbstractLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
@@ -22,12 +25,28 @@ public abstract class AjaxRemovableListView<T extends Serializable> extends List
 
     public AjaxRemovableListView(String id, List<? extends T> list) {
         super(id, list);
+        init();
+    }
+
+    public AjaxRemovableListView(String id, IModel<? extends List<? extends T>> model) {
+        super(id, model);
+        init();
+    }
+
+    protected AjaxRemovableListView init() {
         setReuseItems(true);
+        return this;
     }
 
     @Override
     protected IModel<T> getListItemModel(IModel<? extends List<T>> listViewModel, int index) {
         return new Model<T>(listViewModel.getObject().get(index));
+    }
+
+    protected AjaxSubmitLink addRemoveSubmitLink(String linkId, Form<?> form, ListItem<T> item, Component toFocus, Component... toUpdate) {
+        AjaxSubmitLink removeSubmitLink = getRemoveSubmitLink(linkId, form, toFocus, toUpdate);
+        item.add(removeSubmitLink);
+        return removeSubmitLink;
     }
 
     protected AjaxLink addRemoveLink(String linkId, ListItem<T> item, Component toFocus, Component... toUpdate) {
@@ -36,49 +55,65 @@ public abstract class AjaxRemovableListView<T extends Serializable> extends List
         return removeLink;
     }
 
+    protected AjaxSubmitLink getRemoveSubmitLink(String linkId, Form<?> form, final Component toFocus, final Component... toUpdate) {
+        AjaxSubmitLink link = new AjaxSubmitLink(linkId, form) {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                updateListViewOnRemoval(this, target, toFocus, toUpdate);
+            }
+        };
+        link.setDefaultFormProcessing(false);
+        return link;
+    }
+
     protected AjaxLink getRemoveLink(String linkId, final Component toFocus, final Component... toUpdate) {
         return new AjaxLink(linkId) {
 
             @Override
             public void onClick(AjaxRequestTarget target) {
-                ListItem<T> item = (ListItem) getParent();
-                ListView<T> list = (ListView<T>) item.getParent();
-
-                int removeIndex = item.getIndex();
-                int last_index = list.getModelObject().size() - 1;
-
-                //Copy childs from next list item and remove last
-                for (int index = item.getIndex(); index < last_index; index++) {
-                    ListItem<T> li = (ListItem) item.getParent().get(index);
-                    ListItem<T> li_next = (ListItem) item.getParent().get(index + 1);
-
-                    li.removeAll();
-                    if (li.getIndex() == removeIndex) {
-                        for (int i = 0; i < li.size(); i++) {
-                            li.get(i).remove();
-                        }
-                    }
-                    li.setModelObject(li_next.getModelObject());
-
-                    int size = li_next.size();
-                    Component[] childs = new Component[size];
-                    for (int i = 0; i < size; i++) {
-                        childs[i] = li_next.get(i);
-                    }
-                    li.add(childs);
-                }
-                item.getParent().get(last_index).remove();
-
-                list.getModelObject().remove(removeIndex);
-
-                if (toFocus != null) {
-                    target.focusComponent(toFocus);
-                }
-
-                for (Component comp : toUpdate) {
-                    target.addComponent(comp);
-                }
+                updateListViewOnRemoval(this, target, toFocus, toUpdate);
             }
         };
+    }
+
+    private static void updateListViewOnRemoval(AbstractLink link, AjaxRequestTarget target, Component toFocus, Component... toUpdate) {
+        ListItem item = (ListItem) link.getParent();
+        ListView list = (ListView) item.getParent();
+
+        int removeIndex = item.getIndex();
+        int last_index = list.getModelObject().size() - 1;
+
+        //Copy childs from next list item and remove last
+        for (int index = item.getIndex(); index < last_index; index++) {
+            ListItem li = (ListItem) item.getParent().get(index);
+            ListItem li_next = (ListItem) item.getParent().get(index + 1);
+
+            li.removeAll();
+            if (li.getIndex() == removeIndex) {
+                for (int i = 0; i < li.size(); i++) {
+                    li.get(i).remove();
+                }
+            }
+            li.setModelObject(li_next.getModelObject());
+
+            int size = li_next.size();
+            Component[] childs = new Component[size];
+            for (int i = 0; i < size; i++) {
+                childs[i] = li_next.get(i);
+            }
+            li.add(childs);
+        }
+        item.getParent().get(last_index).remove();
+
+        list.getModelObject().remove(removeIndex);
+
+        if (toFocus != null) {
+            target.focusComponent(toFocus);
+        }
+
+        for (Component comp : toUpdate) {
+            target.addComponent(comp);
+        }
     }
 }
