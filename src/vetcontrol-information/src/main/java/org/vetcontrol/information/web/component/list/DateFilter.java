c9c0ed4502dccc419ae4293c9f2579663c53437f
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-16 05:13:55 +0000

    certain component classes was moved or renamed. Feedback support for errors (for example in case of wrong date value) in BookPage was added.

--- src/vetcontrol-information/src/main/java/org/vetcontrol/information/web/component/list/DateFilter.java
+++ src/vetcontrol-information/src/main/java/org/vetcontrol/information/web/component/list/DateFilter.java
@@ -2,13 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.vetcontrol.information.web.component.edit;
+package org.vetcontrol.information.web.component.list;
 
 import java.util.Date;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.AbstractFilter;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
 import org.apache.wicket.model.IModel;
 import org.odlabs.wiquery.ui.datepicker.DatePicker;
+import org.vetcontrol.information.web.model.DisplayPropertyLocalizableModel;
+import org.vetcontrol.util.book.Property;
 
 /**
  *
@@ -18,9 +20,10 @@ public class DateFilter extends AbstractFilter {
 
     private DatePicker<Date> filter;
 
-    public DateFilter(String id, IModel model, FilterForm form) {
+    public DateFilter(String id, IModel model, FilterForm form, Property property) {
         super(id, form);
         filter = new DatePicker<Date>("filter", model);
+        filter.setLabel(new DisplayPropertyLocalizableModel(property, this));
         filter.setShowOn(DatePicker.ShowOnEnum.FOCUS);
         enableFocusTracking(filter);
         add(filter);
--- src/vetcontrol-information/src/main/java/org/vetcontrol/information/web/component/list/ModifyColumnFilter.java
+++ src/vetcontrol-information/src/main/java/org/vetcontrol/information/web/component/list/ModifyColumnFilter.java
@@ -5,9 +5,16 @@ import org.apache.wicket.markup.html.panel.Panel;
 
 class ModifyColumnFilter extends Panel {
 
-    public ModifyColumnFilter(String id) {
+    public ModifyColumnFilter(String id, final IBookDataProvider dataProvider) {
         super(id);
-        Button goSearch = new Button("filter");
+        Button goSearch = new Button("filter"){
+
+            @Override
+            public void onSubmit() {
+                dataProvider.initSize();
+            }
+
+        };
         add(goSearch);
     }
 }
