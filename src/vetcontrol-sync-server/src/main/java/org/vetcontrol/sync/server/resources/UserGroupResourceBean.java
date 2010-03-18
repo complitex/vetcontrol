3f1aaa116a0ca8b6ccc3f95c89de4eadb9c704a9
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-18 04:58:19 +0000

    'order by' clause was added.

--- src/vetcontrol-sync-server/src/main/java/org/vetcontrol/sync/server/resources/UserGroupResourceBean.java
+++ src/vetcontrol-sync-server/src/main/java/org/vetcontrol/sync/server/resources/UserGroupResourceBean.java
@@ -28,23 +28,22 @@ import java.util.ResourceBundle;
  * @author Anatoly A. Ivanov java@inheaven.ru
  *         Date: 17.02.2010 14:46:24
  */
-@Stateless @Path("/usergroup")
+@Stateless
+@Path("/usergroup")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserGroupResourceBean {
+
     private static final Logger log = LoggerFactory.getLogger(UserResourceBean.class);
     private static final ResourceBundle rb = ResourceBundle.getBundle("org.vetcontrol.sync.server.resources.ResourceBeans");
-
     @PersistenceContext
     private EntityManager em;
-
     @EJB(beanName = "ClientBean")
     private ClientBean clientBean;
-
     @EJB(beanName = "LogBean")
     private LogBean logBean;
 
-    private Client getClient(SyncRequestEntity requestEntity, HttpServletRequest request){
+    private Client getClient(SyncRequestEntity requestEntity, HttpServletRequest request) {
         try {
             return clientBean.getClient(requestEntity.getSecureKey());
         } catch (Exception e) {
@@ -53,24 +52,19 @@ public class UserGroupResourceBean {
             logBean.error(Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, UserGroupResourceBean.class, Client.class,
                     rb.getString("error.secure_key.check") + "[ip: {0}]", request.getRemoteHost());
 
-            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN)
-                    .entity(rb.getString("error.secure_key.check"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
+            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN).entity(rb.getString("error.secure_key.check")).type("text/plain;charset=UTF-8").build());
         }
     }
 
-    @POST @Path("/list")
-    public GenericEntity<List<UserGroup>> getUserGroups(SyncRequestEntity requestEntity, @Context HttpServletRequest r){
+    @POST
+    @Path("/list")
+    public GenericEntity<List<UserGroup>> getUserGroups(SyncRequestEntity requestEntity, @Context HttpServletRequest r) {
         Client client = getClient(requestEntity, r);
 
-        List<UserGroup> list = em.createQuery("select ug from UserGroup ug, User u " +
-                "where ug.login = u.login and u.department = :department and ug.updated >= :updated", UserGroup.class)
-                .setParameter("department", client.getDepartment())
-                .setParameter("updated", requestEntity.getUpdated())
-                .getResultList();
+        List<UserGroup> list = em.createQuery("select ug from UserGroup ug, User u "
+                + "where ug.login = u.login and u.department = :department and ug.updated >= :updated order by ug.updated", UserGroup.class).setParameter("department", client.getDepartment()).setParameter("updated", requestEntity.getUpdated()).getResultList();
 
-        if (!list.isEmpty()){
+        if (!list.isEmpty()) {
             logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, UserGroupResourceBean.class, UserGroup.class,
                     rb.getString("info.sync.processed"), list.size(),
                     r.getRemoteHost(), client.getIp());
@@ -79,48 +73,44 @@ public class UserGroupResourceBean {
                     new Object[]{client.getId(), list.size(), r.getRemoteHost(), client.getIp()});
         }
 
-        return new GenericEntity<List<UserGroup>>(list){};
+        return new GenericEntity<List<UserGroup>>(list) {
+        };
     }
 
-    @POST @Path("/count")
-    public Count getUserGroupsCount(SyncRequestEntity requestEntity, @Context HttpServletRequest r){
-        return new Count(em.createQuery("select count(ug) from UserGroup ug, User u " +
-                "where ug.login = u.login and u.department = :department and ug.updated >= :updated", Long.class)
-                .setParameter("department", getClient(requestEntity, r).getDepartment())
-                .setParameter("updated", requestEntity.getUpdated())
-                .getSingleResult().intValue());
+    @POST
+    @Path("/count")
+    public Count getUserGroupsCount(SyncRequestEntity requestEntity, @Context HttpServletRequest r) {
+        return new Count(em.createQuery("select count(ug) from UserGroup ug, User u "
+                + "where ug.login = u.login and u.department = :department and ug.updated >= :updated", Long.class).setParameter("department", getClient(requestEntity, r).getDepartment()).setParameter("updated", requestEntity.getUpdated()).getSingleResult().intValue());
     }
 
-    @POST @Path("/deleted/list")
-    public GenericEntity<List<DeletedLongId>> getDeletedUserGroups(SyncRequestEntity requestEntity, @Context HttpServletRequest r){
+    @POST
+    @Path("/deleted/list")
+    public GenericEntity<List<DeletedLongId>> getDeletedUserGroups(SyncRequestEntity requestEntity, @Context HttpServletRequest r) {
         Client client = getClient(requestEntity, r);
 
-        List<DeletedLongId> list = em.createQuery("select d from DeletedLongId d " +
-                "where d.id.entity = :entity and d.deleted >= :updated", DeletedLongId.class)
-                .setParameter("entity", UserGroup.class.getCanonicalName())
-                .setParameter("updated", requestEntity.getUpdated())
-                .getResultList();
+        List<DeletedLongId> list = em.createQuery("select d from DeletedLongId d "
+                + "where d.id.entity = :entity and d.deleted >= :updated", DeletedLongId.class).setParameter("entity", UserGroup.class.getCanonicalName()).setParameter("updated", requestEntity.getUpdated()).getResultList();
 
-        if (!list.isEmpty()){
+        if (!list.isEmpty()) {
             logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, UserGroupResourceBean.class, UserGroup.class,
                     rb.getString("info.sync.processed"), list.size(),
                     r.getRemoteHost(), client.getIp());
 
-            log.info("Синхронизация удаленных групп пользователей. " + rb.getString("info.sync.processed.log"), 
+            log.info("Синхронизация удаленных групп пользователей. " + rb.getString("info.sync.processed.log"),
                     new Object[]{client.getId(), list.size(), r.getRemoteHost(), client.getIp()});
         }
 
-        return new GenericEntity<List<DeletedLongId>>(list){};
+        return new GenericEntity<List<DeletedLongId>>(list) {
+        };
     }
 
-    @POST @Path("/deleted/count")
-    public Count getDeletedUserGroupsCount(SyncRequestEntity requestEntity, @Context HttpServletRequest r){
+    @POST
+    @Path("/deleted/count")
+    public Count getDeletedUserGroupsCount(SyncRequestEntity requestEntity, @Context HttpServletRequest r) {
         getClient(requestEntity, r);
 
-        return new Count( em.createQuery("select count(*) from DeletedLongId d " +
-                "where d.id.entity = :entity and d.deleted >= :updated", Long.class)
-                .setParameter("entity", UserGroup.class.getCanonicalName())
-                .setParameter("updated", requestEntity.getUpdated())
-                .getSingleResult().intValue());
+        return new Count(em.createQuery("select count(*) from DeletedLongId d "
+                + "where d.id.entity = :entity and d.deleted >= :updated", Long.class).setParameter("entity", UserGroup.class.getCanonicalName()).setParameter("updated", requestEntity.getUpdated()).getSingleResult().intValue());
     }
 }
