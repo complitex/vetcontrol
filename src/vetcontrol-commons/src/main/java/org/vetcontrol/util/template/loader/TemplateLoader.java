0e9224d90f500ba151db7b45753d9a1ee47fd351
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-19 09:33:31 +0000

    vetcontrol-template.xml was moved into separated vetcontrol-configuration module, log4j-over-slf4j was added to vetcontrol-client in order to be able to support logging of libraries that use log4j.

---
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/util/template/loader/TemplateLoader.java
@@ -0,0 +1,69 @@
+package org.vetcontrol.util.template.loader;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ * Date: 22.12.2009 18:33:27
+ *
+ * Загружает список меню из файла конфигурации.
+ */
+public class TemplateLoader {
+
+    private List<String> menuClassNames;
+
+    public TemplateLoader(InputStream inputStream) {
+        try {
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse(inputStream);
+
+            menuClassNames = getFragmentsClassName(document);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<String> getFragmentsClassName(Document document) {
+        NodeList sidebar = document.getElementsByTagName("sidebar");
+        if (sidebar.getLength() == 0) {
+            return null;
+        }
+
+        List<String> menuClassNames = new ArrayList<String>();
+
+        NodeList fragments = sidebar.item(0).getChildNodes();
+
+        for (int i = 0; i < fragments.getLength(); i++) {
+            if (fragments.item(i) instanceof Element) {
+                Element menu = (Element) fragments.item(i);
+                if ("menu".equals(menu.getTagName())) {
+                    String className = menu.getAttribute("class");
+                    if (className.length() > 0) {
+                        menuClassNames.add(className);
+                    }
+                }
+            }
+        }
+
+        return menuClassNames;
+    }
+
+    public List<String> getMenuClassNames() {
+        return menuClassNames;
+    }
+}
--- src/vetcontrol-commons/src/main/java/org/vetcontrol/web/template/TemplateWebApplication.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/web/template/TemplateWebApplication.java
@@ -8,6 +8,13 @@ import org.vetcontrol.web.security.ServeltAuthWebApplication;
 import org.wicketstuff.javaee.injection.JavaEEComponentInjector;
 
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vetcontrol.util.template.loader.TemplateLoader;
 
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
@@ -17,24 +24,58 @@ import java.io.InputStream;
  */
 public abstract class TemplateWebApplication extends ServeltAuthWebApplication {
 
-    private final static String templatePath = "/WEB-INF/vetcontrol-template.xml";
-    private TemplateLoader templateLoader;
+    private static final Logger log = LoggerFactory.getLogger(TemplateWebApplication.class);
+    private static final String TEMPLATE_CONFIG_FILE_NAME = "vetcontrol-template.xml";
+    private List<Class<ITemplateMenu>> menuClasses;
 
     @Override
     protected void init() {
         super.init();
 
-        addComponentInstantiationListener(new WiQueryInstantiationListener());
-        addComponentInstantiationListener(new JavaEEComponentInjector(this));
+        initializeWiQuery();
+        initializeJEEInjector();
+        initializeTemplateConfig();
+    }
 
-        InputStream inputStream = getServletContext().getResourceAsStream(templatePath);
-        if (inputStream != null) {
-            templateLoader = new TemplateLoader(inputStream);
+    private void initializeTemplateConfig() throws RuntimeException {
+        try {
+            Iterator<URL> resources = getApplicationSettings().getClassResolver().getResources(TEMPLATE_CONFIG_FILE_NAME);
+            InputStream inputStream = null;
+            if (resources.hasNext()) {
+                inputStream = resources.next().openStream();
+                if (resources.hasNext()) {
+                    log.warn("There are more than one template config {} files. What file will be picked is unpredictable.", TEMPLATE_CONFIG_FILE_NAME);
+                }
+            } else {
+                throw new RuntimeException("Template config file " + TEMPLATE_CONFIG_FILE_NAME + " was not found.");
+            }
+            TemplateLoader templateLoader = new TemplateLoader(inputStream);
+            List<String> menuClassNames = templateLoader.getMenuClassNames();
+
+            menuClasses = new ArrayList<Class<ITemplateMenu>>();
+            for (String className : menuClassNames) {
+                try {
+                    Class menuClass = getApplicationSettings().getClassResolver().resolveClass(className);
+                    menuClasses.add(menuClass);
+                } catch (Exception e) {
+                    log.warn("Меню не найдено [{}]", e.getLocalizedMessage());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public TemplateLoader getTemplateLoader() {
-        return templateLoader;
+    private void initializeJEEInjector() {
+        addComponentInstantiationListener(new JavaEEComponentInjector(this));
+    }
+
+    private void initializeWiQuery() {
+        addComponentInstantiationListener(new WiQueryInstantiationListener());
+    }
+
+    public List<Class<ITemplateMenu>> getMenuClasses() {
+        return menuClasses;
     }
 
     @Override
