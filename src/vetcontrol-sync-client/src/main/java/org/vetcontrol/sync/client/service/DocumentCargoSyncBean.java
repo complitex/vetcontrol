69da5c13c90c02cdfe182fe605eff9609bccd5d6
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-30 08:21:38 +0000

    additional confirmation commit request was eliminated so that document synchronization protocol became more easy. The scope of trnasactions now is more narrow.

--- src/vetcontrol-sync-client/src/main/java/org/vetcontrol/sync/client/service/DocumentCargoSyncBean.java
+++ src/vetcontrol-sync-client/src/main/java/org/vetcontrol/sync/client/service/DocumentCargoSyncBean.java
@@ -6,16 +6,19 @@ import org.vetcontrol.entity.*;
 import org.vetcontrol.service.ClientBean;
 import org.vetcontrol.sync.SyncCargo;
 import org.vetcontrol.sync.SyncDocumentCargo;
-import org.vetcontrol.sync.SyncRequestEntity;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Date;
 import java.util.List;
+import javax.annotation.Resource;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.transaction.UserTransaction;
+import org.vetcontrol.sync.client.service.exception.DBOperationException;
+import org.vetcontrol.sync.client.service.exception.NetworkConnectionException;
 
 import static org.vetcontrol.sync.client.service.ClientFactory.createJSONClient;
 
@@ -24,120 +27,186 @@ import static org.vetcontrol.sync.client.service.ClientFactory.createJSONClient;
  *         Date: 04.03.2010 14:11:26
  */
 @Singleton(name = "DocumentCargoSyncBean")
-public class DocumentCargoSyncBean extends SyncInfo{
-    private static final Logger log = LoggerFactory.getLogger(UserSyncBean.class);
-    private static final int MAX_RESULTS = 100;
+@TransactionManagement(TransactionManagementType.BEAN)
+public class DocumentCargoSyncBean extends SyncInfo {
 
-    @EJB(beanName = "ClientBean")
+    private static final Logger log = LoggerFactory.getLogger(DocumentCargoSyncBean.class);
+    private static final int NETWORK_BATCH = 100;
+    @EJB
     private ClientBean clientBean;
-
     @PersistenceContext
     private EntityManager em;
+    @Resource
+    private UserTransaction ut;
 
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void process(){
+    public void process() {
         processDocumentCargo();
         processCargo();
     }
 
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    private void processDocumentCargo(){
+    private void processDocumentCargo() {
         String secureKey = clientBean.getCurrentSecureKey();
 
         log.debug("\n==================== Synchronizing: DocumentCargo ============================");
 
         start(new SyncEvent(0, DocumentCargo.class));
 
-        em.createQuery("update DocumentCargo set syncStatus = :newSyncStatus where syncStatus = :oldSyncStatus")
-                .setParameter("newSyncStatus", Synchronized.SyncStatus.PROCESSING)
-                .setParameter("oldSyncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED)
-                .executeUpdate();
+        //TODO: why? This value is not used on the server side.
+        Date maxUpdated = getMaxUpdated(DocumentCargo.class);
 
-        List<DocumentCargo> documentCargos = em.createQuery("select dc from DocumentCargo dc " +
-                "where dc.syncStatus = :syncStatus", DocumentCargo.class)
-                .setParameter("syncStatus", Synchronized.SyncStatus.PROCESSING)
-                .getResultList();
+        int count = em.createQuery("select count(dc) from DocumentCargo dc where dc.syncStatus = :syncStatus", Long.class).
+                setParameter("syncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED).
+                getSingleResult().intValue();
 
-        int size = documentCargos.size();
 
-        if (size > 0) {
-            for (int i = 0; i <= size/MAX_RESULTS; ++i) {
-                int from = i*MAX_RESULTS < size ? i*MAX_RESULTS : size-1;
-                int to  = (i+1)*MAX_RESULTS < size ? (i+1)*MAX_RESULTS : size;
+        boolean isNotSynchronizedExist = count > 0;
+        int i = 0;
+        while (isNotSynchronizedExist) {
+            List<DocumentCargo> documents = em.createQuery("select dc from DocumentCargo dc where dc.syncStatus = :syncStatus order by dc.updated",
+                    DocumentCargo.class).
+                    setParameter("syncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED).
+                    setFirstResult(0).
+                    setMaxResults(NETWORK_BATCH).
+                    getResultList();
+            if (documents != null && !documents.isEmpty()) {
+                try {
+                    ut.begin();
+                    for (DocumentCargo dc : documents) {
+                        dc.setSyncStatus(Synchronized.SyncStatus.SYNCHRONIZED);
+                        em.merge(dc);
+                    }
+                    ut.commit();
+                } catch (Exception e) {
+                    try {
+                        ut.rollback();
+                    } catch (Exception rollbackExc) {
+                        log.error("Couldn't to rollback transaction.", rollbackExc);
+                    }
+                    throw new DBOperationException("DB operation exception.", e, DocumentCargo.class, Log.EVENT.SYNC);
+                }
 
-                List<DocumentCargo> subList = documentCargos.subList(from, to);
+                try {
+                    createJSONClient("/document/document_cargo").put(new SyncDocumentCargo(secureKey, maxUpdated, documents));
+                    sync(new SyncEvent(count, i * NETWORK_BATCH, DocumentCargo.class));
+                } catch (Exception e) {
+                    StringBuilder query = new StringBuilder("update DocumentCargo dc set dc.syncStatus = :newSyncStatus"
+                            + " where dc.id.id in (");
+                    for (int j = 0; j < documents.size(); j++) {
+                        query.append(documents.get(j).getId());
+                        if (j < documents.size() - 1) {
+                            query.append(", ");
+                        }
+                    }
+                    query.append(")");
 
-                createJSONClient("/document/document_cargo").put(new SyncDocumentCargo(secureKey,
-                        getUpdated(DocumentCargo.class), subList));
-
-                sync(new SyncEvent(size, from, DocumentCargo.class));
-
-                em.createQuery("update DocumentCargo set syncStatus = :newSyncStatus where syncStatus = :oldSyncStatus")
-                        .setParameter("newSyncStatus", Synchronized.SyncStatus.SYNCHRONIZED)
-                        .setParameter("oldSyncStatus", Synchronized.SyncStatus.PROCESSING)
-                        .executeUpdate();
-
-                sync(new SyncEvent(size, size, DocumentCargo.class));
-
-                createJSONClient("/document/document_cargo/commit").put(new SyncRequestEntity(secureKey,
-                        getUpdated(DocumentCargo.class)));
+                    try {
+                        ut.begin();
+                        em.createQuery(query.toString()).
+                                setParameter("newSyncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED).
+                                executeUpdate();
+                        ut.commit();
+                    } catch (Exception updateExc) {
+                        log.error("Couldn't to recover documents state.", updateExc);
+                        try {
+                            ut.rollback();
+                        } catch (Exception rollbackExc) {
+                            log.error("Couldn't to rollback transaction.", rollbackExc);
+                        }
+                    }
+                    throw new NetworkConnectionException("Network connection exception.", e, DocumentCargo.class, Log.EVENT.SYNC);
+                }
+                i++;
+            } else {
+                isNotSynchronizedExist = false;
             }
         }
 
-        complete(new SyncEvent(size, DocumentCargo.class));
+        complete(new SyncEvent(count, DocumentCargo.class));
         log.debug("++++++++++++++++++++ Synchronizing Complete: DocumentCargo +++++++++++++++++++\n");
     }
 
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    private void processCargo(){
+    private void processCargo() {
         String secureKey = clientBean.getCurrentSecureKey();
 
         log.debug("\n==================== Synchronizing: Cargo ============================");
 
         start(new SyncEvent(0, Cargo.class));
 
-        em.createQuery("update Cargo set syncStatus = :newSyncStatus where syncStatus = :oldSyncStatus")
-                .setParameter("newSyncStatus", Synchronized.SyncStatus.PROCESSING)
-                .setParameter("oldSyncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED)
-                .executeUpdate();
+        //TODO: why? This value is not used on the server side.
+        Date maxUpdated = getMaxUpdated(Cargo.class);
 
-        List<Cargo> cargos = em.createQuery("select c from Cargo c " +
-                "where c.syncStatus = :syncStatus", Cargo.class)
-                .setParameter("syncStatus", Synchronized.SyncStatus.PROCESSING)
-                .getResultList();
+        int count = em.createQuery("select count(c) from Cargo c where c.syncStatus = :syncStatus", Long.class).
+                setParameter("syncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED).
+                getSingleResult().intValue();
 
-        int size = cargos.size();
+        boolean isNotSynchronizedExist = count > 0;
+        int i = 0;
+        while (isNotSynchronizedExist) {
+            List<Cargo> cargos = em.createQuery("select c from Cargo c where c.syncStatus = :syncStatus order by c.updated", Cargo.class).
+                    setParameter("syncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED).
+                    setFirstResult(0).
+                    setMaxResults(NETWORK_BATCH).
+                    getResultList();
+            if (cargos != null && !cargos.isEmpty()) {
+                try {
+                    ut.begin();
+                    for (Cargo c : cargos) {
+                        c.setSyncStatus(Synchronized.SyncStatus.SYNCHRONIZED);
+                        em.merge(c);
+                    }
+                    ut.commit();
+                } catch (Exception e) {
+                    try {
+                        ut.rollback();
+                    } catch (Exception rollbackExc) {
+                        log.error("Couldn't to rollback transaction.", rollbackExc);
+                    }
+                    throw new DBOperationException("DB operation exception.", e, Cargo.class, Log.EVENT.SYNC);
+                }
 
-        if (size > 0) {
-            for (int i = 0; i <= size/MAX_RESULTS; ++i) {
-                int from = i*MAX_RESULTS < size ? i*MAX_RESULTS : size-1;
-                int to  = (i+1)*MAX_RESULTS < size ? (i+1)*MAX_RESULTS : size;
+                try {
+                    createJSONClient("/document/cargo").put(new SyncCargo(secureKey, maxUpdated, cargos));
+                    sync(new SyncEvent(count, i * NETWORK_BATCH, Cargo.class));
+                } catch (Exception e) {
+                    StringBuilder query = new StringBuilder("update Cargo c set c.syncStatus = :newSyncStatus"
+                            + " where c.id.id in (");
+                    for (int j = 0; j < cargos.size(); j++) {
+                        query.append(cargos.get(j).getId());
+                        if (j < cargos.size() - 1) {
+                            query.append(", ");
+                        }
+                    }
+                    query.append(")");
 
-                List<Cargo> subList = cargos.subList(from, to);
-
-                createJSONClient("/document/cargo").put(new SyncCargo(secureKey, getUpdated(Cargo.class), subList));
-
-                sync(new SyncEvent(size, from, Cargo.class));
-
-                em.createQuery("update Cargo set syncStatus = :newSyncStatus where syncStatus = :oldSyncStatus")
-                        .setParameter("newSyncStatus", Synchronized.SyncStatus.SYNCHRONIZED)
-                        .setParameter("oldSyncStatus", Synchronized.SyncStatus.PROCESSING)
-                        .executeUpdate();
-
-                sync(new SyncEvent(size, size, Cargo.class));
-
-                createJSONClient("/document/cargo/commit").put(new SyncRequestEntity(secureKey, getUpdated(Cargo.class)));
+                    try {
+                        ut.begin();
+                        em.createQuery(query.toString()).
+                                setParameter("newSyncStatus", Synchronized.SyncStatus.NOT_SYNCHRONIZED).
+                                executeUpdate();
+                        ut.commit();
+                    } catch (Exception updateExc) {
+                        log.error("Couldn't to recover cargos state.", updateExc);
+                        try {
+                            ut.rollback();
+                        } catch (Exception rollbackExc) {
+                            log.error("Couldn't to rollback transaction.", rollbackExc);
+                        }
+                    }
+                    throw new NetworkConnectionException("Network connection exception.", e, Cargo.class, Log.EVENT.SYNC);
+                }
+                i++;
+            } else {
+                isNotSynchronizedExist = false;
             }
         }
 
-        complete(new SyncEvent(size, Cargo.class));
+        complete(new SyncEvent(count, Cargo.class));
         log.debug("++++++++++++++++++++ Synchronizing Complete: DocumentCargo +++++++++++++++++++\n");
     }
 
-    private Date getUpdated(Class<? extends IUpdated> entity){
-        Date updated = em.createQuery("select max(e.updated) from " + entity.getSimpleName() +" e", Date.class)
-                .getSingleResult();
-        if (updated == null){
+    private Date getMaxUpdated(Class<? extends IUpdated> entity) {
+        Date updated = em.createQuery("select max(e.updated) from " + entity.getSimpleName() + " e", Date.class).getSingleResult();
+        if (updated == null) {
             return new Date(0);
         }
 
