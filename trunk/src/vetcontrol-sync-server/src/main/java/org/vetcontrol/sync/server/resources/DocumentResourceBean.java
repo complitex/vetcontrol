088ed6fb90352268ea558a429d2d1a18370b8354
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-03-30 08:21:38 +0000

    additional confirmation commit request was eliminated so that document synchronization protocol became more easy. The scope of trnasactions now is more narrow.

--- trunk/src/vetcontrol-sync-server/src/main/java/org/vetcontrol/sync/server/resources/DocumentResourceBean.java
+++ trunk/src/vetcontrol-sync-server/src/main/java/org/vetcontrol/sync/server/resources/DocumentResourceBean.java
@@ -1,5 +1,6 @@
 package org.vetcontrol.sync.server.resources;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vetcontrol.entity.*;
@@ -19,176 +20,113 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ResourceBundle;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
  *         Date: 04.03.2010 12:56:10
  */
 @Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
 @Path("/document")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class DocumentResourceBean {
-    private static final Logger log = LoggerFactory.getLogger(RegistrationResourceBean.class);
-    private static final ResourceBundle rb = ResourceBundle.getBundle("org.vetcontrol.sync.server.resources.ResourceBeans");
 
+    private static final Logger log = LoggerFactory.getLogger(DocumentResourceBean.class);
+    private static final ResourceBundle rb = ResourceBundle.getBundle("org.vetcontrol.sync.server.resources.ResourceBeans");
     @PersistenceContext
     private EntityManager em;
-
-    @EJB(name = "LogBean", beanName = "LogBean")
+    @EJB
     private LogBean logBean;
-
-    @EJB(beanName = "ClientBean")
+    @EJB
     private ClientBean clientBean;
 
-    @PUT @Path("/document_cargo")
-    public void putDocumentCargos(SyncDocumentCargo syncDocumentCargo, @Context HttpServletRequest r){
+    @PUT
+    @Path("/document_cargo")
+    public void putDocumentCargos(SyncDocumentCargo syncDocumentCargo, @Context HttpServletRequest r) {
         Client client = getClient(syncDocumentCargo, r);
 
-        int size = syncDocumentCargo.getDocumentCargos().size();
+        List<DocumentCargo> documents = syncDocumentCargo.getDocumentCargos();
+        if (documents != null && !documents.isEmpty()) {
+            int size = documents.size();
 
-        try {
-            for (DocumentCargo documentCargo : syncDocumentCargo.getDocumentCargos()){
-                if (documentCargo.getId() == null) continue;
+            try {
+                for (DocumentCargo documentCargo : syncDocumentCargo.getDocumentCargos()) {
+                    if (documentCargo.getId() == null) {
+                        continue;
+                    }
+                    securityCheck(client, documentCargo, r);
 
-                securityCheck(client, documentCargo, r);
+                    documentCargo.setClient(em.getReference(Client.class, documentCargo.getClient().getId()));
+                    documentCargo.setDepartment(em.getReference(Department.class, documentCargo.getDepartment().getId()));
 
-                documentCargo.setSyncStatus(Synchronized.SyncStatus.PROCESSING);
-                
-                documentCargo.setClient(em.getReference(Client.class, documentCargo.getClient().getId()));
-                documentCargo.setDepartment(em.getReference(Department.class, documentCargo.getDepartment().getId()));
+                    em.merge(documentCargo);
+                }
 
-                em.merge(documentCargo);
+                logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, DocumentCargo.class,
+                        rb.getString("info.sync.processed"), size, r.getRemoteHost(), client.getIp());
+
+                log.info("Синхронизация карточек на груз. " + rb.getString("info.sync.processed.log"),
+                        new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
+            } catch (WebApplicationException e) {
+                throw e;
+            } catch (Exception e) {
+                logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, DocumentCargo.class,
+                        rb.getString("info.sync.processed"), size, r.getRemoteHost(), client.getIp());
+
+                log.error("Синхронизация карточек на груз. " + rb.getString("info.sync.processed.log"),
+                        new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
+                log.error(e.getLocalizedMessage(), e);
+
+                throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(rb.getString("error.db.internal_server_error")).type("text/plain;charset=UTF-8").build());
             }
-
-            logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, DocumentCargo.class,
-                    rb.getString("info.sync.processed"), size, r.getRemoteHost(), client.getIp());
-
-            log.info("Синхронизация карточек на груз. " + rb.getString("info.sync.processed.log"),
-                    new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
-        }catch (WebApplicationException e){            
-            throw e;
-        } catch (Exception e) {
-            logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, BookResourceBean.class, DocumentCargo.class,
-                    rb.getString("info.sync.processed"), size, r.getRemoteHost(), client.getIp());
-
-            log.error("Синхронизация карточек на груз. " + rb.getString("info.sync.processed.log"),
-                    new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
-            log.error(e.getLocalizedMessage(), e);
-
-            throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(rb.getString("error.db.internal_server_error"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
         }
     }
 
-    @PUT @Path("/document_cargo/commit")
-    public void commitDocumentCargos(SyncRequestEntity syncRequestEntity, @Context HttpServletRequest r ){
-        Client client = getClient(syncRequestEntity, r);
-
-        try {
-            em.createQuery("update DocumentCargo set syncStatus = :newSyncStatus where syncStatus = :oldSyncStatus " +
-                    "and client = :client")
-                    .setParameter("newSyncStatus", Synchronized.SyncStatus.SYNCHRONIZED)
-                    .setParameter("oldSyncStatus", Synchronized.SyncStatus.PROCESSING)
-                    .setParameter("client", client)
-                    .executeUpdate();
-
-            logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC_COMMIT, DocumentResourceBean.class, DocumentCargo.class,
-                    rb.getString("info.sync.commit"), r.getRemoteHost(), client.getIp());
-
-            log.info("Подтверждение синхронизации карточки на груз. " + rb.getString("info.sync.commit.log"),
-                    new Object[]{client.getId(), r.getRemoteHost(), client.getIp()});
-        } catch (Exception e) {
-            logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC_COMMIT, DocumentResourceBean.class, DocumentCargo.class,
-                    rb.getString("info.sync.commit"), r.getRemoteHost(), client.getIp());
-
-            log.error("Ошибка подтверждения синхронизации карточки на груз. " + rb.getString("info.sync.commit.log"),
-                    new Object[]{client.getId(), r.getRemoteHost(), client.getIp()});
-            log.error(e.getLocalizedMessage(), e);
-
-            throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(rb.getString("error.db.internal_server_error"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
-        }
-    }
-
-    @PUT @Path("/cargo")
-    public void putCargos(SyncCargo syncCargo, @Context HttpServletRequest r){
+    @PUT
+    @Path("/cargo")
+    public void putCargos(SyncCargo syncCargo, @Context HttpServletRequest r) {
         Client client = getClient(syncCargo, r);
 
-        int size = syncCargo.getCargos().size();
+        List<Cargo> cargos = syncCargo.getCargos();
+        if (cargos != null && !cargos.isEmpty()) {
+            int size = cargos.size();
 
-        try {
-            for (Cargo cargo : syncCargo.getCargos()){
-                if (cargo.getId() == null) continue;
+            try {
+                for (Cargo cargo : syncCargo.getCargos()) {
+                    if (cargo.getId() == null) {
+                        continue;
+                    }
+                    securityCheck(client, cargo, r);
 
-                securityCheck(client, cargo, r);
+                    cargo.setClient(em.getReference(Client.class, cargo.getClient().getId()));
+                    cargo.setDepartment(em.getReference(Department.class, cargo.getDepartment().getId()));
 
-                cargo.setSyncStatus(Synchronized.SyncStatus.PROCESSING);
-                cargo.setClient(em.getReference(Client.class, cargo.getClient().getId()));
-                cargo.setDepartment(em.getReference(Department.class, cargo.getDepartment().getId()));
+                    em.merge(cargo);
+                }
 
-                em.merge(cargo);
+                logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, DocumentCargo.class,
+                        rb.getString("info.sync.processed"), syncCargo.getCargos().size(),
+                        r.getRemoteHost(), client.getIp());
+
+                log.info("Синхронизация грузов. " + rb.getString("info.sync.processed.log"),
+                        new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
+            } catch (Exception e) {
+                logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, Cargo.class,
+                        rb.getString("info.sync.processed"), size, r.getRemoteHost(), client.getIp());
+
+                log.error("Ошибка синхронизации грузов. " + rb.getString("info.sync.processed.log"),
+                        new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
+                log.error(e.getLocalizedMessage(), e);
+
+                throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(rb.getString("error.db.internal_server_error")).type("text/plain;charset=UTF-8").build());
             }
-
-            logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, DocumentCargo.class,
-                    rb.getString("info.sync.processed"), syncCargo.getCargos().size(),
-                    r.getRemoteHost(), client.getIp());
-
-            log.info("Синхронизация грузов. " + rb.getString("info.sync.processed.log"),
-                    new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
-        } catch (Exception e) {
-            logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, Cargo.class,
-                    rb.getString("info.sync.processed"), size, r.getRemoteHost(), client.getIp());
-
-            log.error("Ошибка синхронизации грузов. " + rb.getString("info.sync.processed.log"),
-                    new Object[]{client.getId(), size, r.getRemoteHost(), client.getIp()});
-            log.error(e.getLocalizedMessage(), e);
-
-            throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(rb.getString("error.db.internal_server_error"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
         }
     }
 
-    @PUT @Path("/cargo/commit")
-    public void commitCargo(SyncRequestEntity syncRequestEntity, @Context HttpServletRequest r){
-        Client client = getClient(syncRequestEntity, r);
-
-        try {
-            em.createQuery("update Cargo set syncStatus = :newSyncStatus where syncStatus = :oldSyncStatus " +
-                    "and client = :client")
-                    .setParameter("newSyncStatus", Synchronized.SyncStatus.SYNCHRONIZED)
-                    .setParameter("oldSyncStatus", Synchronized.SyncStatus.PROCESSING)
-                    .setParameter("client", client)
-                    .executeUpdate();
-
-            logBean.info(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC_COMMIT, DocumentResourceBean.class, DocumentCargo.class,
-                    rb.getString("info.sync.commit"), r.getRemoteHost(), client.getIp());
-
-            log.info("Подтверждение синхронизации грузов. " + rb.getString("info.sync.commit.log"),
-                    new Object[]{client.getId(), r.getRemoteHost(), client.getIp()});
-
-        } catch (Exception e) {
-            logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC_COMMIT, DocumentResourceBean.class, DocumentCargo.class,
-                    rb.getString("info.sync.commit"), r.getRemoteHost(), client.getIp());
-
-            log.error("Ошибка подтверждения синхронизации грузов. " + rb.getString("info.sync.commit.log"),
-                    new Object[]{client.getId(), r.getRemoteHost(), client.getIp()});
-            log.error(e.getLocalizedMessage(), e);
-
-            throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(rb.getString("error.db.internal_server_error"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
-        }
-    }
-
-    private <T extends SyncRequestEntity> Client getClient(T re, HttpServletRequest r){
+    private <T extends SyncRequestEntity> Client getClient(T re, HttpServletRequest r) {
         try {
             return clientBean.getClient(re.getSecureKey());
         } catch (Exception e) {
@@ -197,38 +135,29 @@ public class DocumentResourceBean {
             logBean.error(Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, Client.class,
                     rb.getString("error.secure_key.check") + "[ip: {0}]", r.getRemoteHost());
 
-            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN)
-                    .entity(rb.getString("error.secure_key.check"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
+            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN).entity(rb.getString("error.secure_key.check")).type("text/plain;charset=UTF-8").build());
         }
     }
 
-    private void securityCheck(Client client, DocumentCargo documentCargo, HttpServletRequest r){
-        if (!documentCargo.getClient().getId().equals(client.getId())){
+    private void securityCheck(Client client, DocumentCargo documentCargo, HttpServletRequest r) {
+        if (!documentCargo.getClient().getId().equals(client.getId())) {
             log.error(rb.getString("error.document.check") + "[ip: {}]", r.getRemoteHost());
 
             logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, DocumentCargo.class,
                     rb.getString("error.document.check") + "[ip: {0}]", r.getRemoteHost());
 
-            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN)
-                    .entity(rb.getString("error.document.check"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
+            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN).entity(rb.getString("error.document.check")).type("text/plain;charset=UTF-8").build());
         }
     }
 
-    private void securityCheck(Client client, Cargo cargo, HttpServletRequest r){
-        if (!cargo.getClient().getId().equals(client.getId())){
+    private void securityCheck(Client client, Cargo cargo, HttpServletRequest r) {
+        if (!cargo.getClient().getId().equals(client.getId())) {
             log.error(rb.getString("error.document.check") + "[ip: {}]", r.getRemoteHost());
 
             logBean.error(client, Log.MODULE.SYNC_SERVER, Log.EVENT.SYNC, DocumentResourceBean.class, Cargo.class,
                     rb.getString("error.document.check") + "[ip: {0}]", r.getRemoteHost());
 
-            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN)
-                    .entity(rb.getString("error.document.check"))
-                    .type("text/plain;charset=UTF-8")
-                    .build());
+            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN).entity(rb.getString("error.document.check")).type("text/plain;charset=UTF-8").build());
         }
     }
 }
