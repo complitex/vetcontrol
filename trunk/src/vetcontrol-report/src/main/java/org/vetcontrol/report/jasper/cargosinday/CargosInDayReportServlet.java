2ff4984e96927c62338a9141a41ed2f278ccc9d4
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-04-28 08:36:23 +0000

    error handling in report servlets has been modified.

--- trunk/src/vetcontrol-report/src/main/java/org/vetcontrol/report/jasper/cargosinday/CargosInDayReportServlet.java
+++ trunk/src/vetcontrol-report/src/main/java/org/vetcontrol/report/jasper/cargosinday/CargosInDayReportServlet.java
@@ -94,13 +94,11 @@ public final class CargosInDayReportServlet extends HttpServlet {
                 case TEXT:
                     reportStream = getClass().getResourceAsStream("text/cargos_in_day_report.jasper");
                     JasperPrint jasperPrint = JasperFillManager.fillReport(reportStream, params, dataSource);
-
                     JRTextExporter textExporter = new JRTextExporter();
                     textExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
                     textExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, servletOutputStream);
                     textExporter.setParameter(JRTextExporterParameter.PAGE_WIDTH, TextExporterConstants.PAGE_WIDTH);
                     textExporter.setParameter(JRTextExporterParameter.PAGE_HEIGHT, TextExporterConstants.PAGE_HEIGHT);
-
                     response.setContentType("text/plain");
                     response.setCharacterEncoding("UTF-8");
                     textExporter.exportReport();
@@ -110,10 +108,14 @@ public final class CargosInDayReportServlet extends HttpServlet {
             String error = ServletUtil.error(e, log);
             response.setContentType("text/plain");
             response.setCharacterEncoding("UTF-8");
-            if (servletOutputStream == null) {
-                servletOutputStream = response.getOutputStream();
+            try {
+                if (servletOutputStream == null) {
+                    servletOutputStream = response.getOutputStream();
+                }
+                servletOutputStream.print(error);
+            } catch (Throwable t) {
+                log.error("", t);
             }
-            servletOutputStream.print(error);
         } finally {
             if (servletOutputStream != null) {
                 servletOutputStream.flush();
