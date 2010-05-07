241f8c8af758933b3fe09968ebb202d449f9166f
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-05-07 09:03:12 +0000

    code was little bit improved.

--- trunk/src/vetcontrol-commons/src/main/java/org/vetcontrol/hibernate/id/IdentityFallbackToAssignedGenerator.java
+++ trunk/src/vetcontrol-commons/src/main/java/org/vetcontrol/hibernate/id/IdentityFallbackToAssignedGenerator.java
@@ -5,8 +5,13 @@
 package org.vetcontrol.hibernate.id;
 
 import java.io.Serializable;
+import java.util.Properties;
+import org.hibernate.MappingException;
+import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.SessionImplementor;
+import org.hibernate.id.Configurable;
 import org.hibernate.id.IdentityGenerator;
+import org.hibernate.type.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,13 +19,19 @@ import org.slf4j.LoggerFactory;
  *
  * @author Artem
  */
-public class IdentityFallbackToAssignedGenerator extends IdentityGenerator {
+public class IdentityFallbackToAssignedGenerator extends IdentityGenerator implements Configurable {
 
     private static final Logger log = LoggerFactory.getLogger(IdentityFallbackToAssignedGenerator.class);
 
+    private String entityName;
+
+    @Override
+    public void configure(Type type, Properties params, Dialect d) throws MappingException {
+        entityName = params.getProperty(ENTITY_NAME);
+    }
+
     @Override
     public Serializable generate(SessionImplementor sessionImplementor, Object object) {
-        String entityName = sessionImplementor.bestGuessEntityName(object);
         Serializable identifier = sessionImplementor.getEntityPersister(entityName, object).getIdentifier(object, sessionImplementor);
 
         log.debug("Class = {} , entity name = {}, identifier object = {}", new Object[]{object.getClass(), entityName, identifier});
