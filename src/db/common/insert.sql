48089cc9027b2bcd1d8c4c1f01c257ecec67378c
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-05-12 03:41:19 +0000

    meat reports were modified.

--- src/db/common/insert.sql
+++ src/db/common/insert.sql
@@ -792,5 +792,6 @@ INSERT INTO `container_validator` (`id`, `prefix`, `carrier_abbr`, `carrier_name
 UPDATE `generator` SET `generatorValue` = 166 WHERE `generatorName` = 'books';
 
 /* reports */
-INSERT INTO `reports`(`id`) VALUES ('MOVEMENT_TYPE_REPORT'), ('CARGOS_IN_DAY_REPORT'), ('REGIONAL_CONTROL_REPORT'), ('ARREST_REPORTS'), ('MEAT_REPORTS');
+INSERT INTO `reports`(`id`) VALUES ('MOVEMENT_TYPES_REPORT'), ('CARGOS_IN_DAY_REPORT'), ('REGIONAL_CONTROL_REPORT'), ('ARREST_REPORT'),
+('EXTENDED_ARREST_REPORT'), ('MEAT_IN_DAY_REPORT'), ('MEAT_IN_YEAR_REPORT');
 
