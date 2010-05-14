3eb4c77d26ca99e6d018422cd147d817d2dcb5d5
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-05-14 09:28:12 +0000

    transformation of jpa entity manager to hibernate session was updated with new version using only jpa api.

--- src/vetcontrol-commons/src/main/java/org/vetcontrol/hibernate/util/HibernateSessionTransformer.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/hibernate/util/HibernateSessionTransformer.java
@@ -6,7 +6,6 @@ package org.vetcontrol.hibernate.util;
 
 import javax.persistence.EntityManager;
 import org.hibernate.Session;
-import org.hibernate.ejb.EntityManagerImpl;
 
 /**
  *
@@ -17,12 +16,18 @@ public final class HibernateSessionTransformer {
     private HibernateSessionTransformer() {
     }
 
-    public static Session getSession(EntityManager em) {
-        if (em instanceof EntityManagerImpl) {
-            return ((EntityManagerImpl) em).getSession();
-        } else if ((em.getDelegate()) instanceof EntityManagerImpl) {
-            return ((EntityManagerImpl) em.getDelegate()).getSession();
-        }
-        throw new RuntimeException("Can't transform entity manager to hibernate session.");
+//    public static Session getSession(EntityManager em) {
+//        if (em instanceof EntityManagerImpl) {
+//            return ((EntityManagerImpl) em).getSession();
+//        } else if ((em.getDelegate()) instanceof EntityManagerImpl) {
+//            return ((EntityManagerImpl) em.getDelegate()).getSession();
+//        }
+//        throw new RuntimeException("Can't transform entity manager to hibernate session.");
+//    }
+    /**
+     * New version of transformation using EntityManager's unwrap method of JPA2 API.
+     */
+    public static Session getSession(EntityManager entityManager) {
+        return entityManager.unwrap(Session.class);
     }
 }
