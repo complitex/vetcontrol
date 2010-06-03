a2ffe9657478e35f6bfd1558117b793f02089f8e
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-06-03 09:59:45 +0000

    load parameters was changed.

---
+++ trunk/src/vetcontrol-load-test/src/main/java/org/vetcontrol/load/Util.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vetcontrol.load;
+
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.vetcontrol.entity.CargoMode;
+import org.vetcontrol.entity.Client;
+import org.vetcontrol.entity.CountryBook;
+import org.vetcontrol.entity.Department;
+import org.vetcontrol.entity.PassingBorderPoint;
+import org.vetcontrol.entity.User;
+import org.vetcontrol.hibernate.util.HibernateSessionTransformer;
+
+/**
+ *
+ * @author Artem
+ */
+public final class Util {
+
+    public static final String PERSISTENCE_UNIT_NAME = "applicationPersistenceUnitTest";
+
+    private Util() {
+    }
+
+    public static Client[] initClients(int clientCount) {
+        try {
+            Client[] clients = new Client[clientCount];
+
+            EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            for (int i = 0; i < clientCount; i++) {
+                Client c = new Client();
+                c.setCreated(new Date());
+                c.setUpdated(new Date());
+                c.setDepartment(newDepartment());
+                c.setIp("0.0.0.0");
+                c.setMac(String.valueOf(i));
+                c.setSecureKey(String.valueOf(i));
+                HibernateSessionTransformer.getSession(entityManager).saveOrUpdate(c);
+                clients[i] = c;
+            }
+
+            transaction.commit();
+            entityManager.close();
+            entityManagerFactory.close();
+
+            return clients;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Department newDepartment() {
+        Department department = new Department();
+        department.setId(1L);
+        return department;
+    }
+
+    public static User newCreator() {
+        User creator = new User();
+        creator.setId(1L);
+        return creator;
+    }
+
+    public static CountryBook newSenderCountry() {
+        CountryBook senderCountry = new CountryBook();
+        senderCountry.setId(1L);
+        return senderCountry;
+    }
+
+    public static CargoMode newCargoMode() {
+        CargoMode cargoMode = new CargoMode();
+        cargoMode.setId(1L);
+        return cargoMode;
+    }
+
+    public static PassingBorderPoint newPassingBorderPoint() {
+        PassingBorderPoint borderPoint = new PassingBorderPoint();
+        borderPoint.setId(1L);
+        return borderPoint;
+    }
+}
---
+++ trunk/src/vetcontrol-load-test/src/main/java/org/vetcontrol/load/Worker.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vetcontrol.load;
+
+import com.sun.jersey.api.client.UniformInterface;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.vetcontrol.entity.Client;
+import org.vetcontrol.sync.SyncRequestEntity;
+
+/**
+ *
+ * @author Artem
+ */
+public abstract class Worker<T> implements Runnable {
+
+    private Long startId;
+
+    private Long endId;
+
+    private Client client;
+
+    private Logger log;
+
+    private int networkBatchCount;
+
+    private String serverPath;
+
+    public Worker(Client client, Long startId, Long endId, Logger log, int networkBatchCount, String serverPath) {
+        this.startId = startId;
+        this.endId = endId;
+        this.client = client;
+        this.log = log;
+        this.networkBatchCount = networkBatchCount;
+        this.serverPath = serverPath;
+    }
+
+    @Override
+    public void run() {
+        log.info("Work started. Client id : {}", client.getId());
+        try {
+            UniformInterface uniformInterface = ClientFactory.createJSONClient(serverPath);
+            Id id = new Id(startId, endId);
+            List<T> documents = new ArrayList<T>(networkBatchCount);
+
+            while (id.canIncrement()) {
+                T document = newDocument(id.getAndIncrement());
+                documents.add(document);
+                if (documents.size() % networkBatchCount == 0) {
+                    postDocuments(uniformInterface, documents);
+                }
+            }
+            if (!documents.isEmpty()) {
+                postDocuments(uniformInterface, documents);
+            }
+        } catch (Exception e) {
+            log.error("Fatal unexpected problem. Client id : " + client.getId(), e);
+        }
+        log.info("Work finished. Client id : {}", client.getId());
+    }
+
+    protected void postDocuments(UniformInterface uniformInterface, List<T> documents) {
+        long startTime = System.currentTimeMillis();
+        try {
+            uniformInterface.put(newRequestEntity(documents));
+            log.info("Successful posting of document cargos took {} sec. Client id : {}", getTimeDifference(startTime) / 1000.0, client.getId());
+            documents.clear();
+        } catch (Exception e) {
+            log.error("Unsuccessful posting of document cargos took " + getTimeDifference(startTime) / 1000.0
+                    + " sec. Could not to post document cargo to server. Client id : " + client.getId(), e);
+        }
+    }
+
+    protected Client getClient(){
+        return client;
+    }
+
+    protected abstract SyncRequestEntity newRequestEntity(List<T> documents);
+
+    protected abstract T newDocument(Long documentId);
+
+    private static long getTimeDifference(long startTime) {
+        return System.currentTimeMillis() - startTime;
+    }
+}
