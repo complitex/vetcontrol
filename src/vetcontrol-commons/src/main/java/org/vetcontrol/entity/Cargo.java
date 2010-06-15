ede4c8c7862d2befba5ea82990348ec9d9e0a833
heaven.iaa <heaven.iaa@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-06-15 12:10:24 +0000

    add document cargo random generation, entity refactor

--- src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/Cargo.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/Cargo.java
@@ -69,6 +69,23 @@ public class Cargo extends Synchronized implements IUpdated{
     @Column(name = "count", nullable = true, precision = 2)
     private Double count;
 
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name="vehicle_id", referencedColumnName = "id", insertable = false, updatable = false),
+            @JoinColumn(name="department_id", referencedColumnName = "department_id", insertable = false, updatable = false),
+            @JoinColumn(name="client_id", referencedColumnName = "client_id", insertable = false, updatable = false)
+    })
+    @XmlIDREF
+    private Vehicle vehicle;
+
+    @Column(name = "vehicle_id", nullable=false)
+    private Long vehicleId;
+
+    @ManyToOne
+    @JoinColumn(name = "cargo_producer_id", nullable = false)
+    @XmlIDREF
+    private CargoProducer cargoProducer;
+
     @Column(name = "certificate_details", length = 255, nullable = false)
     private String certificateDetails;
 
@@ -79,23 +96,6 @@ public class Cargo extends Synchronized implements IUpdated{
     @Temporal(TemporalType.TIMESTAMP)
     private Date updated;
 
-    @ManyToOne
-    @JoinColumn(name = "cargo_producer_id", nullable = false)
-    @XmlIDREF
-    private CargoProducer cargoProducer;
-
-    @ManyToOne
-    @JoinColumns({
-        @JoinColumn(name="vehicle_id", referencedColumnName = "id", insertable = false, updatable = false),
-        @JoinColumn(name="department_id", referencedColumnName = "department_id", insertable = false, updatable = false),
-        @JoinColumn(name="client_id", referencedColumnName = "client_id", insertable = false, updatable = false)
-    })
-    @XmlIDREF
-    private Vehicle vehicle;
-
-    @Column(name = "vehicle_id", nullable=false)
-    private Long vehicleId;
-
     @PrePersist @PreUpdate
     protected void preUpdate(){
         if (documentCargo != null){
@@ -105,6 +105,8 @@ public class Cargo extends Synchronized implements IUpdated{
             department = documentCargo.getDepartment();
         }
     }
+
+    // AUTO GENERATED Getter and Setter
 
     public Long getId() {
         return id;
@@ -146,7 +148,6 @@ public class Cargo extends Synchronized implements IUpdated{
         this.documentCargoId = documentCargoId;
     }
 
-
     public CargoType getCargoType() {
         return cargoType;
     }
@@ -171,40 +172,6 @@ public class Cargo extends Synchronized implements IUpdated{
         this.count = count;
     }
 
-    public String getCertificateDetails() {
-        return certificateDetails;
-    }
-
-    public void setCertificateDetails(String certificateDetails) {
-        this.certificateDetails = certificateDetails;
-    }
-
-    public Date getCertificateDate() {
-        return certificateDate;
-    }
-
-    public void setCertificateDate(Date certificateDate) {
-        this.certificateDate = certificateDate;
-    }
-
-    @Override
-    public Date getUpdated() {
-        return updated;
-    }
-
-    @Override
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
-
-    public CargoProducer getCargoProducer() {
-        return cargoProducer;
-    }
-
-    public void setCargoProducer(CargoProducer cargoProducer) {
-        this.cargoProducer = cargoProducer;
-    }
-
     public Vehicle getVehicle() {
         return vehicle;
     }
@@ -221,6 +188,40 @@ public class Cargo extends Synchronized implements IUpdated{
         this.vehicleId = vehicleId;
     }
 
+    public CargoProducer getCargoProducer() {
+        return cargoProducer;
+    }
+
+    public void setCargoProducer(CargoProducer cargoProducer) {
+        this.cargoProducer = cargoProducer;
+    }
+
+    public String getCertificateDetails() {
+        return certificateDetails;
+    }
+
+    public void setCertificateDetails(String certificateDetails) {
+        this.certificateDetails = certificateDetails;
+    }
+
+    public Date getCertificateDate() {
+        return certificateDate;
+    }
+
+    public void setCertificateDate(Date certificateDate) {
+        this.certificateDate = certificateDate;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    // AUTO GENERATED Equal and HashCode
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -229,20 +230,15 @@ public class Cargo extends Synchronized implements IUpdated{
 
         Cargo cargo = (Cargo) o;
 
-        if (cargoProducer != null ? !cargoProducer.equals(cargo.cargoProducer) : cargo.cargoProducer != null)
-            return false;
+        if (cargoProducer != null ? !cargoProducer.equals(cargo.cargoProducer) : cargo.cargoProducer != null) return false;
         if (cargoType != null ? !cargoType.equals(cargo.cargoType) : cargo.cargoType != null) return false;
-        if (certificateDate != null ? !certificateDate.equals(cargo.certificateDate) : cargo.certificateDate != null)
-            return false;
-        if (certificateDetails != null ? !certificateDetails.equals(cargo.certificateDetails) : cargo.certificateDetails != null)
-            return false;
+        if (certificateDate != null ? !certificateDate.equals(cargo.certificateDate) : cargo.certificateDate != null) return false;
+        if (certificateDetails != null ? !certificateDetails.equals(cargo.certificateDetails) : cargo.certificateDetails != null) return false;
         if (client != null ? !client.equals(cargo.client) : cargo.client != null) return false;
         if (count != null ? !count.equals(cargo.count) : cargo.count != null) return false;
         if (department != null ? !department.equals(cargo.department) : cargo.department != null) return false;
-        if (documentCargo != null ? !documentCargo.equals(cargo.documentCargo) : cargo.documentCargo != null)
-            return false;
-        if (documentCargoId != null ? !documentCargoId.equals(cargo.documentCargoId) : cargo.documentCargoId != null)
-            return false;
+        if (documentCargo != null ? !documentCargo.equals(cargo.documentCargo) : cargo.documentCargo != null) return false;
+        if (documentCargoId != null ? !documentCargoId.equals(cargo.documentCargoId) : cargo.documentCargoId != null) return false;
         if (id != null ? !id.equals(cargo.id) : cargo.id != null) return false;
         if (unitType != null ? !unitType.equals(cargo.unitType) : cargo.unitType != null) return false;
         if (updated != null ? !updated.equals(cargo.updated) : cargo.updated != null) return false;
@@ -263,13 +259,13 @@ public class Cargo extends Synchronized implements IUpdated{
         result = 31 * result + (cargoType != null ? cargoType.hashCode() : 0);
         result = 31 * result + (unitType != null ? unitType.hashCode() : 0);
         result = 31 * result + (count != null ? count.hashCode() : 0);
+        result = 31 * result + (vehicle != null ? vehicle.hashCode() : 0);
+        result = 31 * result + (vehicleId != null ? vehicleId.hashCode() : 0);
+        result = 31 * result + (cargoProducer != null ? cargoProducer.hashCode() : 0);
         result = 31 * result + (certificateDetails != null ? certificateDetails.hashCode() : 0);
         result = 31 * result + (certificateDate != null ? certificateDate.hashCode() : 0);
         result = 31 * result + (updated != null ? updated.hashCode() : 0);
-        result = 31 * result + (cargoProducer != null ? cargoProducer.hashCode() : 0);
-        result = 31 * result + (vehicle != null ? vehicle.hashCode() : 0);
-        result = 31 * result + (vehicleId != null ? vehicleId.hashCode() : 0);
         return result;
-    }   
+    }
 }
 
--- src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/DocumentCargo.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/entity/DocumentCargo.java
@@ -47,17 +47,6 @@ public class DocumentCargo extends Synchronized implements IUpdated {
     @XmlIDREF
     private Department department;
 
-    @ManyToOne
-    @JoinColumn(name = "creator_id")
-    @XmlIDREF
-    private User creator;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date created;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date updated;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "movement_type", nullable = false)
     private MovementType movementType;
@@ -66,39 +55,52 @@ public class DocumentCargo extends Synchronized implements IUpdated {
     @Column(name = "vehicle_type", nullable = false)
     private VehicleType vehicleType;
 
-    @OneToMany(mappedBy = "documentCargo")
-    @OrderBy("id")
-    @XmlTransient
-    private List<Cargo> cargos = new ArrayList<Cargo>();
-
-    @OneToMany(mappedBy = "documentCargo")
-    @XmlTransient
-    private List<Vehicle> vehicles = new ArrayList<Vehicle>();
-
     @ManyToOne
-    @JoinColumn(name = "cargo_sender_country_id")
+    @JoinColumn(name = "cargo_sender_country_id", nullable = false)
     private CountryBook senderCountry;
 
-    @Column(name = "cargo_sender_name")
+    @Column(name = "cargo_sender_name", nullable = false)
     private String senderName;
 
-    @Column(name = "cargo_receiver_address")
+    @Column(name = "cargo_receiver_address", nullable = false)
     private String receiverAddress;
 
-    @Column(name = "cargo_receiver_name")
+    @Column(name = "cargo_receiver_name", nullable = false)
     private String receiverName;
+
+    @Column(name = "details", length = 1024)
+    private String details;
 
     @ManyToOne
     @JoinColumn(name = "passing_border_point_id")
     @XmlIDREF
     private PassingBorderPoint passingBorderPoint;
 
-    @Column(name = "details", length = 255)
-    private String details;
+    @ManyToOne
+    @JoinColumn(name = "creator_id", nullable = false)
+    @XmlIDREF
+    private User creator;
+
+    @OneToMany(mappedBy = "documentCargo")
+    @XmlTransient
+    private List<Vehicle> vehicles = new ArrayList<Vehicle>();
 
     @ManyToOne
     @JoinColumn(name = "cargo_mode_id", nullable = false)
     private CargoMode cargoMode;
+
+    @OneToMany(mappedBy = "documentCargo")
+    @OrderBy("id")
+    @XmlTransient
+    private List<Cargo> cargos = new ArrayList<Cargo>();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated;
+
+    // AUTO GENERATED Getter and Setter
 
     public Long getId() {
         return id;
@@ -124,30 +126,6 @@ public class DocumentCargo extends Synchronized implements IUpdated {
         this.department = department;
     }
 
-    public User getCreator() {
-        return creator;
-    }
-
-    public void setCreator(User creator) {
-        this.creator = creator;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Date getUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
-
     public MovementType getMovementType() {
         return movementType;
     }
@@ -162,22 +140,6 @@ public class DocumentCargo extends Synchronized implements IUpdated {
 
     public void setVehicleType(VehicleType vehicleType) {
         this.vehicleType = vehicleType;
-    }
-
-    public List<Cargo> getCargos() {
-        return cargos;
-    }
-
-    public void setCargos(List<Cargo> cargos) {
-        this.cargos = cargos;
-    }
-
-    public List<Vehicle> getVehicles() {
-        return vehicles;
-    }
-
-    public void setVehicles(List<Vehicle> vehicles) {
-        this.vehicles = vehicles;
     }
 
     public CountryBook getSenderCountry() {
@@ -212,6 +174,14 @@ public class DocumentCargo extends Synchronized implements IUpdated {
         this.receiverName = receiverName;
     }
 
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
     public PassingBorderPoint getPassingBorderPoint() {
         return passingBorderPoint;
     }
@@ -220,12 +190,20 @@ public class DocumentCargo extends Synchronized implements IUpdated {
         this.passingBorderPoint = passingBorderPoint;
     }
 
-    public String getDetails() {
-        return details;
+    public User getCreator() {
+        return creator;
     }
 
-    public void setDetails(String details) {
-        this.details = details;
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
     }
 
     public CargoMode getCargoMode() {
@@ -236,76 +214,63 @@ public class DocumentCargo extends Synchronized implements IUpdated {
         this.cargoMode = cargoMode;
     }
 
+    public List<Cargo> getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(List<Cargo> cargos) {
+        this.cargos = cargos;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
     public String getDisplayId() {
         return (department != null ? department.getId() : "0") + "."
                 + (client != null ? client.getId() : "0") + "." + id;
     }
 
+
+    // AUTO GENERATED Equal and HashCode 
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof DocumentCargo)) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
+        if (this == o) return true;
+        if (!(o instanceof DocumentCargo)) return false;
+        if (!super.equals(o)) return false;
 
         DocumentCargo that = (DocumentCargo) o;
 
-        if (cargoMode != null ? !cargoMode.equals(that.cargoMode) : that.cargoMode != null) {
-            return false;
-        }
-        if (cargos != null ? !new ArrayList<Cargo>(cargos).equals(that.cargos) : that.cargos != null) {
-            return false;
-        }
-        if (client != null ? !client.equals(that.client) : that.client != null) {
-            return false;
-        }
-        if (created != null ? !created.equals(that.created) : that.created != null) {
-            return false;
-        }
-        if (creator != null ? !creator.equals(that.creator) : that.creator != null) {
-            return false;
-        }
-        if (department != null ? !department.equals(that.department) : that.department != null) {
-            return false;
-        }
-        if (details != null ? !details.equals(that.details) : that.details != null) {
-            return false;
-        }
-        if (id != null ? !id.equals(that.id) : that.id != null) {
-            return false;
-        }
-        if (movementType != null ? !movementType.equals(that.movementType) : that.movementType != null) {
-            return false;
-        }
-        if (passingBorderPoint != null ? !passingBorderPoint.equals(that.passingBorderPoint) : that.passingBorderPoint != null) {
-            return false;
-        }
-        if (receiverAddress != null ? !receiverAddress.equals(that.receiverAddress) : that.receiverAddress != null) {
-            return false;
-        }
-        if (receiverName != null ? !receiverName.equals(that.receiverName) : that.receiverName != null) {
-            return false;
-        }
-        if (senderCountry != null ? !senderCountry.equals(that.senderCountry) : that.senderCountry != null) {
-            return false;
-        }
-        if (senderName != null ? !senderName.equals(that.senderName) : that.senderName != null) {
-            return false;
-        }
-        if (updated != null ? !updated.equals(that.updated) : that.updated != null) {
-            return false;
-        }
-        if (vehicleType != that.vehicleType) {
-            return false;
-        }
-        if (vehicles != null ? !new ArrayList<Vehicle>(vehicles).equals(that.vehicles) : that.vehicles != null) {
-            return false;
-        }
+        if (cargoMode != null ? !cargoMode.equals(that.cargoMode) : that.cargoMode != null) return false;
+        if (cargos != null ? !cargos.equals(that.cargos) : that.cargos != null) return false;
+        if (client != null ? !client.equals(that.client) : that.client != null) return false;
+        if (created != null ? !created.equals(that.created) : that.created != null) return false;
+        if (creator != null ? !creator.equals(that.creator) : that.creator != null) return false;
+        if (department != null ? !department.equals(that.department) : that.department != null) return false;
+        if (details != null ? !details.equals(that.details) : that.details != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (movementType != that.movementType) return false;
+        if (passingBorderPoint != null ? !passingBorderPoint.equals(that.passingBorderPoint) : that.passingBorderPoint != null) return false;
+        if (receiverAddress != null ? !receiverAddress.equals(that.receiverAddress) : that.receiverAddress != null) return false;
+        if (receiverName != null ? !receiverName.equals(that.receiverName) : that.receiverName != null) return false;
+        if (senderCountry != null ? !senderCountry.equals(that.senderCountry) : that.senderCountry != null) return false;
+        if (senderName != null ? !senderName.equals(that.senderName) : that.senderName != null) return false;
+        if (updated != null ? !updated.equals(that.updated) : that.updated != null) return false;
+        if (vehicleType != that.vehicleType) return false;
+        if (vehicles != null ? !vehicles.equals(that.vehicles) : that.vehicles != null) return false;
 
         return true;
     }
@@ -316,20 +281,20 @@ public class DocumentCargo extends Synchronized implements IUpdated {
         result = 31 * result + (id != null ? id.hashCode() : 0);
         result = 31 * result + (client != null ? client.hashCode() : 0);
         result = 31 * result + (department != null ? department.hashCode() : 0);
-        result = 31 * result + (creator != null ? creator.hashCode() : 0);
-        result = 31 * result + (created != null ? created.hashCode() : 0);
-        result = 31 * result + (updated != null ? updated.hashCode() : 0);
         result = 31 * result + (movementType != null ? movementType.hashCode() : 0);
         result = 31 * result + (vehicleType != null ? vehicleType.hashCode() : 0);
-        result = 31 * result + (cargos != null ? new ArrayList<Cargo>(cargos).hashCode() : 0);
-        result = 31 * result + (vehicles != null ? new ArrayList<Vehicle>(vehicles).hashCode() : 0);
         result = 31 * result + (senderCountry != null ? senderCountry.hashCode() : 0);
         result = 31 * result + (senderName != null ? senderName.hashCode() : 0);
         result = 31 * result + (receiverAddress != null ? receiverAddress.hashCode() : 0);
         result = 31 * result + (receiverName != null ? receiverName.hashCode() : 0);
-        result = 31 * result + (passingBorderPoint != null ? passingBorderPoint.hashCode() : 0);
         result = 31 * result + (details != null ? details.hashCode() : 0);
+        result = 31 * result + (passingBorderPoint != null ? passingBorderPoint.hashCode() : 0);
+        result = 31 * result + (creator != null ? creator.hashCode() : 0);
+        result = 31 * result + (vehicles != null ? vehicles.hashCode() : 0);
         result = 31 * result + (cargoMode != null ? cargoMode.hashCode() : 0);
+        result = 31 * result + (cargos != null ? cargos.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        result = 31 * result + (updated != null ? updated.hashCode() : 0);
         return result;
     }
 }
