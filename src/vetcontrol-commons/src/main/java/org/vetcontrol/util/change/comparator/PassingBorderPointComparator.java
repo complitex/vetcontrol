c24140e74d17a177b7ee8f0a93d488721c555679
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-06-17 10:46:24 +0000

    PassingBoderPointComparator was moved to commons module due to its usage in user module.

--- src/vetcontrol-commons/src/main/java/org/vetcontrol/util/change/comparator/PassingBorderPointComparator.java
+++ src/vetcontrol-commons/src/main/java/org/vetcontrol/util/change/comparator/PassingBorderPointComparator.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.vetcontrol.document.util.change.comparator;
+package org.vetcontrol.util.change.comparator;
 
 import java.util.Collections;
 import java.util.Set;
