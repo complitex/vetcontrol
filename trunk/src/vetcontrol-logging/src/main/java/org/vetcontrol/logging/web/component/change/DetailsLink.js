72fb7731f27f00263205e0e857a0bb70e1011480
artkof.work <artkof.work@1d30cab6-e88b-11de-b362-3513cf04e977>
2010-06-21 09:53:45 +0000

    details/hide switch was added to log details link.

--- trunk/src/vetcontrol-logging/src/main/java/org/vetcontrol/logging/web/component/change/DetailsLink.js
+++ trunk/src/vetcontrol-logging/src/main/java/org/vetcontrol/logging/web/component/change/DetailsLink.js
@@ -4,10 +4,21 @@ if(typeof(logging) == "undefined"){
 if(typeof(logging.DetailsLink) == "undefined"){
     logging.DetailsLink = {
 
-//        EXPANDED_CLASS: "change_details_expanded",
+        //        EXPANDED_CLASS: "change_details_expanded",
 
         details: function(event){
-            var detailsRow = logging.DetailsLink.getDetailsRow($(event.target));
+            var detailsLink = $(event.target);
+
+            detailsLink.closest(".logging_DetailsLink").find(".message").each(function(index, element){
+                var message = $(element);
+                if(message.css("display") == "none"){
+                    message.show();
+                } else {
+                    message.hide();
+                }
+            });
+
+            var detailsRow = logging.DetailsLink.getDetailsRow(detailsLink);
             var details = logging.DetailsLink.getDetailsSection(detailsRow);
 
             var isHidden = logging.DetailsLink.isHidden(detailsRow);
@@ -15,25 +26,26 @@ if(typeof(logging.DetailsLink) == "undefined"){
 
             if(isHidden){
                 detailsRow.show();
-//                logging.DetailsLink.switchVisibility(detailsRow);
+                //                logging.DetailsLink.switchVisibility(detailsRow);
                 details.toggle('drop', {}, 1000);
             } else {
                 details.toggle('drop', {}, 1000, function(){
-//                    logging.DetailsLink.switchVisibility(detailsRow);
+                    //                    logging.DetailsLink.switchVisibility(detailsRow);
                     detailsRow.hide();
                 });
-            }
+            }            
+            
             return false;
         },
 
         isHidden: function(detailsRow){
             return $(detailsRow).is(":hidden");
-//            return !$(detailsRow).hasClass(logging.DetailsLink.EXPANDED_CLASS);
+        //            return !$(detailsRow).hasClass(logging.DetailsLink.EXPANDED_CLASS);
         },
 
-//        switchVisibility: function(detailsRow){
-//            $(detailsRow).toggleClass(logging.DetailsLink.EXPANDED_CLASS);
-//        },
+        //        switchVisibility: function(detailsRow){
+        //            $(detailsRow).toggleClass(logging.DetailsLink.EXPANDED_CLASS);
+        //        },
 
         getDetailsSection: function(detailsRow){
             return $(detailsRow).find(".logging_DetailsPanel");
@@ -53,17 +65,13 @@ if(typeof(logging.DetailsLink) == "undefined"){
                     alert(message);
                 }
             }
-        },
-
-        init: function(){
-            $(document).ready(function(){
-                $(".logging_DetailsLink").each(function(index, detailsLink){
-                    logging.DetailsLink.getDetailsSection(logging.DetailsLink.getDetailsRow(detailsLink)).hide();
-                });
-                $(".logging_DetailsLink").click(logging.DetailsLink.details);
-            });
         }
     }
 }
 
-logging.DetailsLink.init();
+$(document).ready(function(){
+    $(".logging_DetailsLink").each(function(index, detailsLink){
+        logging.DetailsLink.getDetailsSection(logging.DetailsLink.getDetailsRow(detailsLink)).hide();
+        $(detailsLink).click(logging.DetailsLink.details);
+    });
+});
